111f60eb771582477274484e831ca848383881a5
Pierre Renaux <pierre@talansoft.com>
2012-09-14 11:01:46 +0800

    Added xslt_tools toolset ;

---
+++ toolsets/xslt_tools/setup-toolset.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# toolset
+export HAM_TOOLSET=XSLT_TOOLS
+export HAM_TOOLSET_VER=1
+export HAM_TOOLSET_NAME=xslt_tools
+export HAM_TOOLSET_DIR=${HAM_HOME}/toolsets/xslt_tools
+
+# path setup
+case $HAM_OS in
+    NT*)
+        export XSLT_TOOLS_BIN_DIR=${HAM_TOOLSET_DIR}/nt-x86/
+        export PATH=${XSLT_TOOLS_BIN_DIR}:${PATH}
+        if [ ! -e "$XSLT_TOOLS_BIN_DIR" ]; then
+            toolset_dl xslt_tools xslt_tools_1_nt-x86
+            if [ ! -e "$XSLT_TOOLS_BIN_DIR" ]; then
+                echo "E/nt-x86 folder doesn't exist in the toolset"
+                return 1
+            fi
+        fi
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
+
+# jars
+export XSLT_TOOLS_JARS_DIR=${HAM_TOOLSET_DIR}/jars/
+if [ ! -e "$XSLT_TOOLS_JARS_DIR" ]; then
+    toolset_dl xslt_tools xslt_tools_1_jars
+    if [ ! -e "$XSLT_TOOLS_JARS_DIR" ]; then
+        echo "E/jars folder doesn't exist in the toolset"
+        return 1
+    fi
+fi
+
+# path
+export PATH=${HAM_TOOLSET_DIR}:${PATH}
+
+# version
+VER="--- xslt_tools_1 ----------------------
+--- xsltproc ---
+`xsltproc --version | grep xsltproc`
+--- saxon ------
+`xslt_tools-saxon 2>&1 | grep Saxon`"
+if [ $? != 0 ]; then
+    echo "E/Can't get version."
+    return 1
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
