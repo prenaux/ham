2d09519ffbe3d9c605079d8601c13bb64c91895c
Pierre Renaux <pierre@talansoft.com>
2012-10-08 14:16:18 +0800

    Added emacs (Emacs 23) toolset ;

---
+++ toolsets/emacs/setup-toolset.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# toolset
+export HAM_TOOLSET_IS_SETUP_EMACS=1
+export HAM_TOOLSET=EMACS
+export HAM_TOOLSET_VER=23
+export HAM_TOOLSET_NAME=emacs
+export HAM_TOOLSET_DIR=${HAM_HOME}/toolsets/emacs
+
+# path setup
+case $HAM_OS in
+    NT*)
+        export EMACS_DIR=${HAM_TOOLSET_DIR}/nt-x86/
+        export PATH=${EMACS_DIR}/bin:${PATH}
+        if [ ! -e $EMACS_DIR ]; then
+            toolset_dl emacs emacs_nt-x86
+            if [ ! -e $EMACS_DIR ]; then
+                echo "E/nt-x86 folder doesn't exist in the toolset"
+                return 1
+            fi
+        fi
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
+
+VER="--- emacs ------------------------
+`emacs --version | grep 'Emacs' | head -n 1`"
+if [ $? != 0 ]; then
+    echo "E/Can't get version."
+    return 1
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
