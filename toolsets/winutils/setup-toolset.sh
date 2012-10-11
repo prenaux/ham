cc7f7e55fe95a5a81b3ce3eb98f67f33ae2ae1c8
Pierre Renaux <pierre@talansoft.com>
2012-10-11 13:32:37 +0800

    Added winutils toolset, contains sysinternals, everything (fast global find file), FileZilla, NetworkMonitor 3.4, notepad++, HexEdit, sniffer, SumatraPDF, depends (view exported symbols and deps of DLLs), CPU-Z, 7zip and CoreTemp.

---
+++ toolsets/winutils/setup-toolset.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# toolset
+export HAM_TOOLSET_IS_SETUP_WINUTILS=1
+export HAM_TOOLSET=WINUTILS
+export HAM_TOOLSET_VER=1
+export HAM_TOOLSET_NAME=winutils
+export HAM_TOOLSET_DIR=${HAM_HOME}/toolsets/winutils
+
+# path setup
+case $HAM_OS in
+    NT*)
+        export WINUTILS_DIR=${HAM_TOOLSET_DIR}/nt-x86/
+        export PATH=${HAM_TOOLSET_DIR}/:${PATH}:${WINUTILS_DIR}/git/bin/:${WINUTILS_DIR}/svn/bin/
+        if [ ! -e "$WINUTILS_DIR" ]; then
+            toolset_dl winutils winutils_nt-x86
+            if [ ! -e "$WINUTILS_DIR" ]; then
+                echo "E/nt-x86 folder doesn't exist in the toolset"
+                return 1
+            fi
+        fi
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
+
+VER="--- winutils ------------------------
+Various Windows utilities: Notepad++, Network Monitor, Sysinternals,
+Process Explorer, Depends, Sniffer, CPU-Z, HexEdit, SumatraPDF, ..."
+if [ $? != 0 ]; then
+    echo "E/Can't get version."
+    return 1
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
