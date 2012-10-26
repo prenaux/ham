3a85038a05da50ed1097de7a2c7c8ba2043ba73d
Pierre Renaux <pierre@talansoft.com>
2012-10-26 23:12:51 +0800

    [rules/toolkit-msvc_11_x86.ham]: Added HAS_PPL ; [toolsets/defaultj]: Imports default after the JDK ;

--- toolsets/defaultj/setup-toolset.sh
+++ toolsets/defaultj/setup-toolset.sh
@@ -3,9 +3,8 @@
 # path setup
 case $HAM_OS in
     NT*)
-        . ham-toolset-import.sh xslt_tools
         . ham-toolset-import.sh java_jdk16
-        . ham-toolset-import.sh msvc_10_x86
+        . ham-toolset-import.sh default
         ;;
     *)
         echo "E/Toolset: Unsupported host OS"
