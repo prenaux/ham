b280751eb1e98abb6023c835870df700ee366cfe
Pierre Renaux <pierre@talansoft.com>
2012-10-26 13:21:54 +0800

    Support VC11 (VS2012) ;

--- toolsets/default/setup-toolset.sh
+++ toolsets/default/setup-toolset.sh
@@ -4,7 +4,14 @@
 case $HAM_OS in
     NT*)
         . ham-toolset-import.sh xslt_tools
-        . ham-toolset-import.sh msvc_10_x86
+        MSVCDIR="`unxpath "$PROGRAMFILES\\Microsoft Visual Studio 11.0\\VC"`"
+        if [ -e "$MSVCDIR/bin/cl.exe" ]; then
+            echo "I/Default Toolset: Detected VS2012"
+            . ham-toolset-import.sh msvc_11_x86
+        else
+            echo "I/Default Toolset: Using VC10 legacy package"
+            . ham-toolset-import.sh msvc_10_x86
+        fi
         ;;
     *)
         echo "E/Toolset: Unsupported host OS"
