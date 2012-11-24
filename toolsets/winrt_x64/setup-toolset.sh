87a51c29be004c39c59a20efc9186f6326b17ce0
Pierre Renaux <pierre@talansoft.com>
2012-11-24 15:24:26 +0800

    [rules & toolsets]: Added support for winrt_arm & winrt_x64 targets ;

--- toolsets/winrt_x64/setup-toolset.sh
+++ toolsets/winrt_x64/setup-toolset.sh
@@ -1,9 +1,9 @@
 . ham-toolset-import.sh xslt_tools
-. ham-toolset-import.sh msvc_11_x86
+. ham-toolset-import.sh msvc_11_x64
 export LIBPATH="`nativedir \"${WINSDKDIR}/References/CommonConfiguration/Neutral\"`;`nativedir \"${MSVCDIR}/vcpackages\"`"
-export HAM_TOOLSET_IS_SETUP_MSVC_11_X86=
-export HAM_TOOLSET_IS_SETUP_WINRT_X86=1
+export HAM_TOOLSET_IS_SETUP_MSVC_11_X64=
+export HAM_TOOLSET_IS_SETUP_WINRT_X64=1
 export HAM_TOOLSET=VISUALC
 export HAM_TOOLSET_VER=11
-export HAM_TOOLSET_NAME=winrt_x86
+export HAM_TOOLSET_NAME=winrt_x64
 export HAM_TOOLSET_DIR=${HAM_HOME}/toolsets/${HAM_TOOLSET_NAME}
