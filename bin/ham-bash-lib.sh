49b5945cd945199de135098cb5c1d089ff34539e
Pierre Renaux <pierre@talansoft.com>
2013-04-21 19:39:20 +0800

    [bin]: ham-bash-lib.sh, added ni-hash functions and the toolset functions ; [bin/nt-x86]: Added env & cut unixy tools ; [bin/ham-toolset-import.sh]: Clear environment first and prevent double import automatically ;

--- bin/ham-bash-lib.sh
+++ bin/ham-bash-lib.sh
@@ -7,6 +7,35 @@ fi
 export CYGWIN=nodosfilewarning
 
 ########################################################################
+##  Tables
+########################################################################
+ni-hput() {
+  eval export "NI_HASH_$1__""$2"='$3'
+}
+
+ni-hremove() {
+  eval unset "NI_HASH_$1__""$2"
+}
+
+ni-hclear() {
+    for h in $(ni-hkeys t); do
+        ni-hremove $1 $h
+    done
+}
+
+ni-hget() {
+  eval echo '${'"NI_HASH_$1__$2"'#hash}'
+}
+
+ni-hkeys() {
+  env | grep -E "^NI_HASH_$1__[[:alnum:]]*=" | sed -r "s/^NI_HASH_$1__(.*)=.*/\\1/g"
+}
+
+ni-hprint() {
+  env | grep -E "^NI_HASH_$1__[[:alnum:]]*=" | sed "s/^NI_HASH_$1__//g"
+}
+
+########################################################################
 ##  Utils
 ########################################################################
 complain()
@@ -119,6 +148,42 @@ $ '
 
 upsearch() {
     test / == "$PWD" && return || test -e "$1" && echo "$PWD" && return || cd .. && upsearch "$1"
+}
+
+########################################################################
+##  Toolsets
+########################################################################
+toolset_import() {
+    export PATH=$PATH
+    . ham-toolset-import.sh $1
+    if [ $? != 0 ]; then
+        return 1
+    fi
+}
+
+toolset_dl() {
+    export CWD=`pwd`
+    export DIR=${HAM_HOME}/toolsets/$1
+    export ARCH_URL1="http://localhost:8123/data/toolsets/$2.7z"
+    export ARCH_URL2="https://bitbucket.org/prenaux/ham/downloads/$2.7z"
+    export DLFILENAME="_$2.7z"
+    cd ${DIR}
+    if [ ! -e "$DLFILENAME" ]; then
+        echo "... Trying download from ${ARCH_URL1}"
+        wget -c --no-check-certificate $ARCH_URL1 -O"$DLFILENAME.wget"
+        if [ $? != 0 ]; then
+            echo "... Trying download from ${ARCH_URL2}"
+            wget -c --no-check-certificate $ARCH_URL2 -O"$DLFILENAME.wget"
+            if [ $? != 0 ]; then
+                echo "Download failed !"
+                return 1;
+            fi
+        fi
+        mv "$DLFILENAME.wget" "$DLFILENAME"
+    fi
+    echo "... Extracting $DLFILENAME"
+    7z x -y $DLFILENAME | grep -v -e "\(7-Zip\|Processing\|Extracting\|^$\)" -
+    cd ${CWD}
 }
 
 ########################################################################
--- bin/ham-toolset-import.sh
+++ bin/ham-toolset-import.sh
@@ -1,40 +1,3 @@
-########################################################################
-toolset_import() {
-    export PATH=$PATH
-    . ham-toolset-import.sh $1
-    if [ $? != 0 ]; then
-        echo "E/Toolset '$1' import failed !"
-        return 1
-    else
-        echo "I/Imported toolset '$1'."
-    fi
-}
-
-toolset_dl() {
-    export CWD=`pwd`
-    export DIR=${HAM_HOME}/toolsets/$1
-    export ARCH_URL1="http://localhost:8123/data/toolsets/$2.7z"
-    export ARCH_URL2="https://bitbucket.org/prenaux/ham/downloads/$2.7z"
-    export DLFILENAME="_$2.7z"
-    cd ${DIR}
-    if [ ! -e "$DLFILENAME" ]; then
-        echo "... Trying download from ${ARCH_URL1}"
-        wget -c --no-check-certificate $ARCH_URL1 -O"$DLFILENAME.wget"
-        if [ $? != 0 ]; then
-            echo "... Trying download from ${ARCH_URL2}"
-            wget -c --no-check-certificate $ARCH_URL2 -O"$DLFILENAME.wget"
-            if [ $? != 0 ]; then
-                echo "Download failed !"
-                return 1;
-            fi
-        fi
-        mv "$DLFILENAME.wget" "$DLFILENAME"
-    fi
-    echo "... Extracting $DLFILENAME"
-    7z x -y $DLFILENAME | grep -v -e "\(7-Zip\|Processing\|Extracting\|^$\)" -
-    cd ${CWD}
-}
-
 export DIR=${HAM_HOME}/toolsets/$1
 export SETUP_SCRIPT=$DIR/setup-toolset.sh
 if [ ! -f "$SETUP_SCRIPT" ]; then
@@ -42,8 +5,18 @@ if [ ! -f "$SETUP_SCRIPT" ]; then
     return 1
 fi
 
-export PATH=$PATH
-. $SETUP_SCRIPT
-if [ $? != 0 ]; then
-    return 1
+ALREADY_IMPORTED=`ni-hget HAM_IMPORTS $1`
+
+if [[ $ALREADY_IMPORTED = "1" ]]; then
+    echo "I/Already imported '$1'."
+else
+    export PATH=$PATH
+    . $SETUP_SCRIPT
+    if [ $? != 0 ]; then
+        echo "E/Toolset '$1' import failed !"
+        return 1
+    else
+        ni-hput HAM_IMPORTS $1 1
+        echo "I/Imported toolset '$1'."
+    fi
 fi
