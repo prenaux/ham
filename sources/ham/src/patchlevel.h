71ae0377219587823bd989bc1e00fbc6ea567409
Pierre Renaux <pierre@talansoft.com>
2013-05-02 15:29:31 +0800

    [ham]: Version 7, re-implemented spawnvp so that it doesnt popup a console window on Windows when ham is ran from within a windowed app ;

--- sources/ham/src/patchlevel.h
+++ sources/ham/src/patchlevel.h
@@ -1,5 +1,5 @@
 /* Keep JAMVERSYM in sync with VERSION. */
 /* It can be accessed as $(JAMVERSION) in the Jamfile. */
 
-#define VERSION "6 (" __DATE__ ")"
-#define JAMVERSYM "JAMVERSION=6"
+#define VERSION "7 (" __DATE__ " - " __TIME__ ")"
+#define JAMVERSYM "JAMVERSION=7"
