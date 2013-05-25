7c3ad3ddc66a23aeabe09b40e06018450411dfa2
Pierre Renaux <pierre@talansoft.com>
2013-05-25 21:07:08 +0800

    [emacs/site/ni-user-pierre.el]: Added my-indent-buffer & my-indent-marked-files functions ;

--- sources/emacs/site/ni-user-pierre.el
+++ sources/emacs/site/ni-user-pierre.el
@@ -62,3 +62,54 @@
 ;;;======================================================================
 (require 'diminish)
 (diminish 'pabbrev-mode)
+
+;;;======================================================================
+;;; Buffer cleanup and indentation
+;;;======================================================================
+(defun xsteve-remove-control-M ()
+  "Remove ^M at end of line in the whole buffer."
+  (interactive)
+  (save-match-data
+    (save-excursion
+      (let ((remove-count 0))
+        (goto-char (point-min))
+        (while (re-search-forward (concat (char-to-string 13) "$") (point-max) t)
+          (setq remove-count (+ remove-count 1))
+          (replace-match "" nil nil))
+        ))))
+
+(defun dos2unix ()
+  "Not exactly but it's easier to remember"
+  (interactive)
+  (set-buffer-file-coding-system 'unix 't)
+  (xsteve-remove-control-M))
+
+(defun remove-dos-eol ()
+  "Do not show ^M in files containing mixed UNIX and DOS line endings."
+  (interactive)
+  (setq buffer-display-table (make-display-table))
+  (aset buffer-display-table ?\^M []))
+
+(defun my-indent-buffer ()
+  (interactive)
+  ;; (begining-of-buffer)
+  (untabify (point-min) (point-max))
+  (dos2unix)
+  (remove-dos-eol)
+  (indent-region (point-min) (point-max)))
+
+(defun my-indent-marked-files ()
+  (interactive)
+  (setq num-files (length (dired-get-marked-files)))
+  (setq count 1)
+  (dolist (file (dired-get-marked-files))
+    (message (format "Indenting %s (%d/%d)" file count num-files))
+    (find-file file)
+    (ignore-errors
+      (my-indent-buffer))
+    (save-buffer)
+    (kill-buffer nil)
+    (setq count (+ count 1))
+    )
+  (message (format "Done indenting %d files" num-files))
+  )
