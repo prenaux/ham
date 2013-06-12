273edcb6a5a63fc495367bbd76f39f92739e481b
Pierre Renaux <pierre@talansoft.com>
2013-06-12 21:26:34 +0800

    [emacs/site/aglemacs.el]: Added unqi-lines function ;

--- sources/emacs/site/aglemacs.el
+++ sources/emacs/site/aglemacs.el
@@ -418,6 +418,25 @@ the text to another HTML buffer."
 ;;;======================================================================
 (agl-begin-time-block "Utils")
 
+(defun uniq-lines (beg end)
+  "Unique lines in region.
+Called from a program, there are two arguments:
+BEG and END (region to sort)."
+  (interactive "r")
+  (save-excursion
+    (save-restriction
+      (narrow-to-region beg end)
+      (goto-char (point-min))
+      (while (not (eobp))
+        (kill-line 1)
+        (yank)
+        (let ((next-line (point)))
+          (while
+              (re-search-forward
+               (format "^%s" (regexp-quote (car kill-ring))) nil t)
+            (replace-match "" nil nil))
+          (goto-char next-line))))))
+
 (defun agl-search-word-backward ()
   "Find the previous occurrence of the current word."
   (interactive)
