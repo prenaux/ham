478942cdee27bf36db0f12e657795190ff2bf406
Pierre Renaux <pierre@talansoft.com>
2013-08-12 17:31:45 +0800

    [bin/ham-search-build-ham.sh]: Search in src & sources in the current folder for the _build.ham file ;

--- bin/ham-search-build-ham.sh
+++ bin/ham-search-build-ham.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 . $HAM_HOME/bin/ham-bash-lib.sh
-upsearch _build.ham
+if test -e "$PWD/sources/_build.ham"; then
+    echo "$PWD/sources"
+elif test -e "$PWD/src/_build.ham"; then
+    echo "$PWD/src"
+else
+    upsearch _build.ham
+fi
