df8c50184816069d46c330efd2ab4dfd2881e9bd
Pierre Renaux <pierre@talansoft.com>
2013-08-26 18:30:20 +0800

    [bin/ham-search-build-ham.sh]: Removed search in src for _build.ham file, as this prevent the compilation from of a module's top level directory (as src is the default directory for C/C++ its always selected when in the module's directory - so we can't build code in other languages or test cases) ;

--- bin/ham-search-build-ham.sh
+++ bin/ham-search-build-ham.sh
@@ -2,8 +2,6 @@
 . $HAM_HOME/bin/ham-bash-lib.sh
 if test -e "$PWD/sources/_build.ham"; then
     echo "$PWD/sources"
-elif test -e "$PWD/src/_build.ham"; then
-    echo "$PWD/src"
 else
     upsearch _build.ham
 fi
