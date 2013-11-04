2b365622cdf36c4711e4050004510a28eae7c253
Pierre Renaux <pierre@talansoft.com>
2013-11-04 22:44:04 +0800

    [rules & toolsets/emscripten]: Updated to latest emscripten build from git ; Updated setup-toolset.sh for Windows ;

--- toolsets/emscripten/setup-toolset.sh
+++ toolsets/emscripten/setup-toolset.sh
@@ -1,28 +1,52 @@
 #!/bin/bash
 
 # import dependencies
+toolset_import repos
+if [ $? != 0 ]; then return 1; fi
+toolset_import clang_33
+if [ $? != 0 ]; then return 1; fi
+toolset_import nodejs_081
+if [ $? != 0 ]; then return 1; fi
+toolset_import java_jdk16
+if [ $? != 0 ]; then return 1; fi
+toolset_import python_27
+if [ $? != 0 ]; then return 1; fi
 toolset_import xslt_tools
 if [ $? != 0 ]; then return 1; fi
 
 # toolset
 export HAM_TOOLSET=EMSCRIPTEN
-export HAM_TOOLSET_VER=3
+export HAM_TOOLSET_VER=2
 export HAM_TOOLSET_NAME=emscripten
 export HAM_TOOLSET_DIR=${HAM_HOME}/toolsets/emscripten
 
-# path setup
-case $HAM_OS in
-    OSX)
-        export PATH=${HAM_TOOLSET_DIR}/osx:${PATH}
-        pushd ${HAM_TOOLSET_DIR}/osx >> /dev/null
-        . emsdk_add_path
-        popd >> /dev/null
-        ;;
-    *)
-        echo "E/Toolset: Unsupported host OS"
+# emscripten setup
+export EMSCRIPTEN_ROOT=${HAM_TOOLSET_DIR}/emscripten
+export LLVM_ROOT=${CLANGDIR}
+export NODE_JS=
+export PATH=${EMSCRIPTEN_ROOT}:$PATH
+
+EMSCRIPTEN_DEFAULT_DOT_FILE=${HAM_TOOLSET_DIR}/etc/.emscripten
+
+# dl if missing
+if [ ! -e $EMSCRIPTEN_ROOT -o ! -e $EMSCRIPTEN_DEFAULT_DOT_FILE ]; then
+    toolset_dl emscripten emscripten
+    if [ ! -e $EMSCRIPTEN_ROOT -o ! -e $EMSCRIPTEN_DEFAULT_DOT_FILE ]; then
+        echo "emscripten folder doesn't exist in the toolset"
         return 1
-        ;;
-esac
+    fi
+fi
+
+# set JVM mem (need for big code base to build and for the JVM to not sometime
+# fail to instantiate)
+export _JAVA_OPTIONS="-Xms256m -Xmx768m"
+
+# copy a configured .emscripten if needed
+if [ ! -f "$HOME/.emscripten" -o ! -f "$HOME/.emscripten_sanity" ]; then
+    echo "# Copying default .emscripten"
+    cp -f $EMSCRIPTEN_DEFAULT_DOT_FILE $HOME/.emscripten
+    emcc --version
+fi
 
 VER="--- emscripten ------------------------
 `emcc --version`"
---
+++ toolsets/python_27/setup-toolset.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# toolset
+export HAM_TOOLSET=PYTHON
+export HAM_TOOLSET_VER=27
+export HAM_TOOLSET_NAME=python_27
+export HAM_TOOLSET_DIR=${HAM_HOME}/toolsets/python_27
+
+# path setup
+case $HAM_OS in
+    NT*)
+        export PYTHON_DIR=${HAM_TOOLSET_DIR}/nt-x86/
+        export PATH=${PYTHON_DIR}:${PYTHON_DIR}/DLLs:${PATH}
+        if [ ! -e "$PYTHON_DIR" ]; then
+            toolset_dl python_27 python_27_nt-x86
+            if [ ! -e "$PYTHON_DIR" ]; then
+                echo "E/nt-x86 folder doesn't exist in the toolset"
+                return 1
+            fi
+        fi
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
+
+VER="--- python_27 ------------------------
+`python --version 2>&1`"
+if [ $? != 0 ]; then
+    echo "E/Can't get version."
+    return 1
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
