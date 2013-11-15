0501c68e6f747c9a26638db111eddb369ff0aad2
Pierre Renaux <pierre@talansoft.com>
2013-11-16 00:57:27 +0800

    [bin/ham-bash-start.sh]: Display WORK environment variable ;

--- bin/ham-bash-start.sh
+++ bin/ham-bash-start.sh
@@ -12,6 +12,7 @@ export BASH_START_PATH=$PATH
 
 if [[ "$BASH_START_SILENT" = "" ]]; then
     echo "=== Ham bash shell ==="
+    echo "WORK = $WORK"
     echo "HAM_HOME = $HAM_HOME"
     echo "HAM_OS = $HAM_OS"
     echo "HAM_BIN_LOA = $HAM_BIN_LOA"
