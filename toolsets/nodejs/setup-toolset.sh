bdc0ca337e18e4607482a24a3b1c79643550b629
Pierre Renaux <pierre@talansoft.com>
2014-01-02 02:30:17 +0800

    [toolsets/nodejs]: Added NodeJS toolset ;

---
+++ toolsets/nodejs/setup-toolset.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# toolset
+export HAM_TOOLSET=NODEJS
+export HAM_TOOLSET_VER=0_10
+export HAM_TOOLSET_NAME=nodejs
+export HAM_TOOLSET_DIR=${HAM_HOME}/toolsets/nodejs
+
+# path setup
+case $HAM_OS in
+    NT*)
+        export NODEJS_DIR=${HAM_TOOLSET_DIR}/nt-x86/
+        export PATH=${NODEJS_DIR}:${PATH}
+        if [ ! -e $NODEJS_DIR ]; then
+            toolset_dl nodejs nodejs_nt-x86
+            if [ ! -e $NODEJS_DIR ]; then
+                echo "E/nt-x86 folder doesn't exist in the toolset"
+                return 1
+            fi
+        fi
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
+
+VER="--- nodejs ------------------------
+`node --version`"
+if [ $? != 0 ]; then
+    echo "E/Can't get version."
+    return 1
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
