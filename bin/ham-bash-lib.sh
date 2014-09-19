f8575b7d61800a4e8fc13f4dca50f106cb1676d1
Pierre Renaux <pierre@talansoft.com>
2014-09-19 17:24:32 +0800

    [bin/ham-bash-lib.sh]: Added rawurlencode bash function ;

--- bin/ham-bash-lib.sh
+++ bin/ham-bash-lib.sh
@@ -163,6 +163,23 @@ upsearch() {
     test / == "$PWD" && return || test -e "$1" && echo "$PWD" && return || cd .. && upsearch "$1"
 }
 
+rawurlencode() {
+  local string="${1}"
+  local strlen=${#string}
+  local encoded=""
+
+  for (( pos=0 ; pos<strlen ; pos++ )); do
+     c=${string:$pos:1}
+     case "$c" in
+        [-_.~a-zA-Z0-9] ) o="${c}" ;;
+        * )               printf -v o '%%%02x' "'$c"
+     esac
+     encoded+="${o}"
+  done
+  echo "${encoded}"    # You can either set a return variable (FASTER)
+  REPLY="${encoded}"   #+or echo the result (EASIER)... or both... :p
+}
+
 ########################################################################
 ##  Toolsets
 ########################################################################
