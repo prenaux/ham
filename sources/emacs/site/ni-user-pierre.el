17b75c9d74e600b18322fd5c845cf120e5d75cb5
Pierre Renaux <pierre@talansoft.com>
2015-04-10 11:30:48 +0800

    [ni-user-pierre.el]: Added .ejs extension to web-mode ;

--- sources/emacs/site/ni-user-pierre.el
+++ sources/emacs/site/ni-user-pierre.el
@@ -358,6 +358,7 @@ If the new path's directories does not exist, create them."
  (add-to-list 'auto-mode-alist '("\\.[gj]sp\\'" . web-mode))
  (add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
  (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+ (add-to-list 'auto-mode-alist '("\\.ejs\\'" . web-mode))
  (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
  (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
  (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
