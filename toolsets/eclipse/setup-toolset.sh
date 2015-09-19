5d2b1cd760e7efc567ba1f4c2d4c3a76b46e1eba
Pierre Renaux <pierre@talansoft.com>
2015-09-19 14:34:42 +0800

    feat: Added eclipse toolset ;

---
+++ toolsets/eclipse/setup-toolset.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# toolset
+export HAM_TOOLSET=ECLIPSE
+export HAM_TOOLSET_VER=1
+export HAM_TOOLSET_NAME=eclipse
+export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/eclipse"
+
+# path setup
+case $HAM_OS in
+    NT*)
+        export ECLIPSE_DIR="${HAM_TOOLSET_DIR}/nt-x86/"
+        export PATH="${ECLIPSE_DIR}/bin":${PATH}
+        if [ ! -e "$ECLIPSE_DIR" ]; then
+            toolset_dl eclipse eclipse_nt-x86
+            if [ ! -e "$ECLIPSE_DIR" ]; then
+                echo "E/nt-x86 folder doesn't exist in the toolset"
+                return 1
+            fi
+        fi
+        ;;
+    OSX*)
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
