faf72c65f09fed00a53278a945dbd23a310fbf5c
Pierre Renaux <pierre@talansoft.com>
2015-11-17 21:29:49 +0800

    feat(toolsets/mysql): Added MySQL 5.7 toolset, Windows only atm ;

---
+++ toolsets/mysql/setup-toolset.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+# toolset
+export HAM_TOOLSET=MYSQL
+export HAM_TOOLSET_VER=57
+export HAM_TOOLSET_NAME=mysql
+export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/mysql"
+
+# path setup
+case $HAM_OS in
+    NT*)
+        export MYSQL_DIR="${HAM_TOOLSET_DIR}/nt-x86/"
+        export PATH="${MYSQL_DIR}/bin":${PATH}
+        if [ ! -e "$MYSQL_DIR" ]; then
+            toolset_dl mysql mysql_nt-x86
+            if [ ! -e "$MYSQL_DIR" ]; then
+                echo "E/nt-x86 folder doesn't exist in the toolset"
+                return 1
+            fi
+        fi
+        ;;
+    OSX*)
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
+
+# path
+export PATH="${HAM_TOOLSET_DIR}":${PATH}
+export MYSQL_DB_DIR="$WORK/Server/mysql"
+
+mkdir -p "$MYSQL_DB_DIR"
+mkdir -p "$MYSQL_DB_DIR/data"
+if [ ! -e "$MYSQL_DB_DIR/my.cnf" ]; then
+    echo [mysqld] > "$MYSQL_DB_DIR/my.cnf"
+    echo basedir=`nativedir "$MYSQL_DB_DIR"` >> "$MYSQL_DB_DIR/my.cnf"
+    echo datadir=`nativedir "$MYSQL_DB_DIR/data"` >> "$MYSQL_DB_DIR/my.cnf"
+    echo character_set_server = utf8 >> "$MYSQL_DB_DIR/my.cnf"
+    echo collation_server = utf8_general_ci >> "$MYSQL_DB_DIR/my.cnf"
+fi
+
+VER="--- mysql ---------------------
+`mysql --version`"
+if [ $? != 0 ]; then
+    echo "E/Can't get version."
+    return 1
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
