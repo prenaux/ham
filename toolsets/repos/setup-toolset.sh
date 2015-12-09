1ff800c155e64e9389be056adceca0504eaa5dab
Pierre Renaux <pierre@talansoft.com>
2015-12-09 11:09:06 +0800

    feat(toolsets/repos): Added git-bfg command ; setup-toolset.sh uses toolset_check_and_dl_ver for its setup ;

--- toolsets/repos/setup-toolset.sh
+++ toolsets/repos/setup-toolset.sh
@@ -2,22 +2,16 @@
 
 # toolset
 export HAM_TOOLSET=REPOS
-export HAM_TOOLSET_VER=1
+export HAM_TOOLSET_VER=3
 export HAM_TOOLSET_NAME=repos
 export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/repos"
 
-# path setup
+# platform
 case $HAM_OS in
     NT*)
+        toolset_check_and_dl_ver repos nt-x86 v3 || return 1
         export REPOS_DIR="${HAM_TOOLSET_DIR}/nt-x86"
         export PATH=${HAM_TOOLSET_DIR}:${PATH}:${REPOS_DIR}/bin/:${REPOS_DIR}/git/bin/:${REPOS_DIR}/svn/bin/:${REPOS_DIR}/hg
-        if [ ! -e "${REPOS_DIR}/git/bin/git.exe" ]; then
-            toolset_dl repos repos_nt-x86
-            if [ ! -e "${REPOS_DIR}/git/bin/git.exe" ]; then
-                echo "E/nt-x86 folder doesn't exist in the toolset"
-                return 1
-            fi
-        fi
         export OPENSSL_CONF="${REPOS_DIR}/git/ssl/openssl.cnf"
         ;;
     OSX*)
@@ -34,6 +28,10 @@ case $HAM_OS in
         ;;
 esac
 
+# jars
+toolset_check_and_dl_ver repos jars v1 || return 1
+
+# version check
 VER="--- repos ------------------------
 --- git ---
 `git --version`"
