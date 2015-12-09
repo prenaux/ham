81a3e4f974fe88cf5718df9dee1209f7db5fae1f
Pierre Renaux <pierre@talansoft.com>
2015-12-09 19:20:01 +0800

    feat(toolsets/scons): Added scons toolset ;

---
+++ sources/scons/tests/HelloScons.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int arcg,char * argv[]) {
+  printf("Hello, Scons!\n");
+  return 0;
+}
---
+++ toolsets/scons/setup-toolset.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+toolset_import python_27
+if [ $? != 0 ]; then return 1; fi
+
+# toolset
+export HAM_TOOLSET=SCONS
+export HAM_TOOLSET_VER=1
+export HAM_TOOLSET_NAME=scons
+export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/scons"
+
+# python
+toolset_check_and_dl_ver scons python v1 || return 1
+
+export PATH="${HAM_HOME}/toolsets/scons/python":$PATH
+
+# version
+VER="--- scons --------------------"
+if [ "$HAM_NO_VER_CHECK" != "1" ]; then
+    VER="$VER
+`scons --version`"
+    if [ $? != 0 ]; then
+        echo "E/Can't get scons version."
+        return 1
+    fi
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
