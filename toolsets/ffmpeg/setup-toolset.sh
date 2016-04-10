b9d902f47a43db12f3eece845521683584bea9f8
Pierre Renaux <pierre@talansoft.com>
2016-04-10 13:14:44 +0800

    feat(toolsets/ffmpeg): Added v2 of the nt-x86 of the ffmpeg toolset which uses the latest FFmpeg release and adds the ffmpeg SDK ;

--- toolsets/ffmpeg/setup-toolset.sh
+++ toolsets/ffmpeg/setup-toolset.sh
@@ -9,15 +9,9 @@ export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/ffmpeg"
 # path setup
 case $HAM_OS in
     NT*)
+        toolset_check_and_dl_ver ffmpeg nt-x86 v2 || return 1
         export FFMPEG_DIR="${HAM_TOOLSET_DIR}/nt-x86/"
         export PATH="${FFMPEG_DIR}":${PATH}
-        if [ ! -e "$FFMPEG_DIR/ffmpeg.exe" ]; then
-            toolset_dl ffmpeg ffmpeg_nt-x86
-            if [ ! -e "$FFMPEG_DIR" ]; then
-                echo "E/nt-x86 folder doesn't exist in the toolset"
-                return 1
-            fi
-        fi
         ;;
     *)
         echo "E/Toolset: Unsupported host OS"
