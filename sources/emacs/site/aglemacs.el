ba5a17d50a4ac7cbf6c5ddaf0b225eda28301db1
Pierre Renaux <pierre@talansoft.com>
2017-07-22 18:21:12 +0800

    feat(emacs/site/aglemacs.el): Logical drag’n drop behavior on macOS ;

--- sources/emacs/site/aglemacs.el
+++ sources/emacs/site/aglemacs.el
@@ -991,3 +991,60 @@ Version 2017-01-27"
           (message "File path copied: 「%s」" -fpath)
           -fpath )))))
 )
+
+;;;======================================================================
+;;; Working drag'n drop on macOS
+;;; Note: The drag'n drop protocol seems to be what's used by Finder
+;;;       and other apps to open files in Emacs
+;;;======================================================================
+(OSX
+ ; Upon drag-and-drop to a frame (OSX window): Find the file in the frame,
+ ;   with shift: insert filename(s), space-separated;
+ ;   with control: insert filename(s) as lines, repeating the beginning of the line;
+ ;   with meta: insert file contents
+ ; note that the emacs window must be activated (CMD-TAB) for the modifiers to register
+ (define-key global-map [M-ns-drag-file] 'ns-insert-file)
+ (define-key global-map [S-ns-drag-file] 'ns-insert-filename)
+ (define-key global-map [C-ns-drag-file] 'ns-insert-filename-as-lines)
+ (define-key global-map [ns-drag-file] 'ns-find-file-in-frame)
+
+ (defun ns-insert-filename ()
+   "Insert contents of first elWindow of `ns-input-file' at point."
+   (interactive)
+   (let ((f (pop ns-input-file)))
+     (insert f))
+   (if ns-input-file                     ; any more? Separate by " "
+       (insert " ")))
+
+ (defun ns-insert-filename-as-lines ()
+   "Insert contents of first elWindow of `ns-input-file' at point,
+as lines repeating any text already on the line before point.."
+   (interactive)
+   (let ((bols (buffer-substring (line-beginning-position) (point))))
+     (let ((f (pop ns-input-file)))
+       (insert f)
+       (insert "\n")
+       (if ns-input-file                     ; any more? Insert bols again
+           (insert bols)))))
+
+ (defun ns-find-file-in-frame ()
+   "Do a `find-file' with the `ns-input-file' as argument; staying in frame."
+   (interactive)
+   (let ((ns-pop-up-frames nil))
+     (ns-find-file)))
+
+ (defun ns-drag-n-drop-other-frame (event)
+   "ns-drag-n-drop-other-frame override to always open file when dragged on Emacs window."
+   (interactive "e")
+   (ns-drag-n-drop event))
+
+ (defun ns-drag-n-drop-as-text (event)
+   "ns-drag-n-drop-other-frame override to always open file when dragged on Emacs window."
+   (interactive "e")
+   (ns-drag-n-drop event))
+
+ (defun ns-drag-n-drop-as-text-other-frame (event)
+   "ns-drag-n-drop-other-frame override to always open file when dragged on Emacs window."
+   (interactive "e")
+   (ns-drag-n-drop event))
+)
