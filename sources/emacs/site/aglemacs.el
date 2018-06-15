f118331df47d4b84734cfba4e9454603037a033a
Pierre Renaux <pierre@talansoft.com>
2018-06-15 22:06:21 +0800

    emacs/site/aglemacs.el: Removed "Overlays" section that is deprecated and doesn't work in Emacs 26 ;

--- sources/emacs/site/aglemacs.el
+++ sources/emacs/site/aglemacs.el
@@ -621,65 +621,6 @@ BEG and END (region to sort)."
 	  t)))))))
 
 ;;;======================================================================
-;;; Overlays
-;;;======================================================================
-(NotBatchMode
- (agl-begin-time-block "Overlays")
- (require 'custom)
-
- (defvar all-overlays ())
-
- (defun agl-delete-this-overlay(overlay is-after begin end &optional len)
-   (delete-overlay overlay)
-   )
-
- (defun agl-highlight-current-line()
-   (interactive)
-   (interactive)
-   (setq current-point (point))
-   (beginning-of-line)
-   (setq beg (point))
-   (forward-line 1)
-   (setq end (point))
-   ;; Create and place the overlay
-   (setq error-line-overlay (make-overlay 1 1))
-   ;; Append to list of all overlays
-   (setq all-overlays (cons error-line-overlay all-overlays))
-
-   (if (= agl-kColorTheme 1)
-       (overlay-put error-line-overlay
-                    'face '(background-color . "#AAEEAA"))
-     (overlay-put error-line-overlay
-                  'face '(background-color . "#115511"))
-     )
-
-   (overlay-put error-line-overlay
-                'modification-hooks (list 'agl-delete-this-overlay))
-   (move-overlay error-line-overlay beg end)
-   (goto-char current-point)
-   )
-
- (defun agl-delete-all-overlays()
-   (interactive)
-   (while all-overlays
-     (delete-overlay (car all-overlays))
-     (setq all-overlays (cdr all-overlays))
-     )
-   )
-
- (defun highlight-error-lines(compilation-buffer, process-result)
-   (interactive)
-   (delete-all-overlays)
-   (condition-case nil
-       (while t
-         (next-error)
-         (highlight-current-line)
-         )
-     (error nil))
-   )
-)
-
-;;;======================================================================
 ;;; Move to visible shell
 ;;;======================================================================
 (NotBatchMode
