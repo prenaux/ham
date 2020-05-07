e9d99e2cff3723bfe1b91bbab6d65540f29d5f36
Pierre Renaux <pierre@talansoft.com>
2020-05-07 13:24:22 +0800

    toolsets/php: Added php_run & php_composer run scripts ; Windows version upgraded to PHP 7.3 ;

--- specs/toolsets/php/setup-toolset.sh
+++ specs/toolsets/php/setup-toolset.sh
@@ -2,22 +2,16 @@
 
 # toolset
 export HAM_TOOLSET=PHP
-export HAM_TOOLSET_VER=56
+export HAM_TOOLSET_VER=73
 export HAM_TOOLSET_NAME=php
 export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/php"
 
 # path setup
 case $HAM_OS in
     NT*)
-        export PHP_DIR="${HAM_TOOLSET_DIR}/nt-x86/"
-        export PATH="${PHP_DIR}":"${HAM_TOOLSET_DIR}":${PATH}
-        if [ ! -e "$PHP_DIR/php.exe" ]; then
-            toolset_dl php php_nt-x86
-            if [ ! -e "$PHP_DIR/php.exe" ]; then
-                echo "E/nt-x86 folder doesn't exist in the toolset"
-                return 1
-            fi
-        fi
+        export PHP_HOME="${HAM_TOOLSET_DIR}/nt-x86/"
+        export PATH="${PHP_HOME}":"${HAM_TOOLSET_DIR}":${PATH}
+        toolset_check_and_dl_ver php nt-x86 v7_3 || return 1
         ;;
     OSX*)
         export PATH="${HAM_TOOLSET_DIR}":${PATH}
@@ -30,7 +24,8 @@ case $HAM_OS in
 esac
 
 VER="--- php ------------------------
-`php --version`"
+`php --version`
+`php_composer -V`"
 if [ $? != 0 ]; then
     echo "E/Can't get version."
     return 1
