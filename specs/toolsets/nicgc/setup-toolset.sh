48849eece43e6d8cd9adbc3b1b5a95a810571ba4
Pierre Renaux <pierre@talansoft.com>
2020-10-25 20:26:22 +0800

    Added nicgc toolset which uses Wine on macOS so that it can use the full Windows build and compile shaders on macOS ;

---
+++ specs/toolsets/nicgc/setup-toolset.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# import
+case $HAM_OS in
+    NT*)
+        ;;
+    OSX*)
+        . ham-toolset-import.sh wine
+        if [ $? != 0 ]; then return 1; fi
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
+
+# toolset
+export HAM_TOOLSET=NICGC
+export HAM_TOOLSET_VER=1
+export HAM_TOOLSET_NAME=nicgc
+export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/nicgc"
+
+# path setup
+case $HAM_OS in
+    NT*)
+        ;;
+    OSX*)
+        toolset_check_and_dl_ver nicgc osx-x64 v1 || return 1
+        export NICGC_DIR="${HAM_TOOLSET_DIR}/osx-x64"
+        export PATH="${NICGC_DIR}/bin":"${NICGC_DIR}/bin":${PATH}
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
+
+# path
+export PATH="${HAM_TOOLSET_DIR}":${PATH}
+
+# version
+VER="--- nicgc ------------------------
+`nicgc -v 2>&1`"
+if [ $? != 0 ]; then
+    echo "E/Can't get nicgc version."
+    return 1
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
---
+++ specs/toolsets/wine/setup-toolset.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# toolset
+export HAM_TOOLSET=wine
+export HAM_TOOLSET_VER=4
+export HAM_TOOLSET_NAME=wine
+export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/wine"
+
+# path setup
+case $HAM_OS in
+    NT*)
+        echo "E/Toolset: Unsupported host OS, you shouldn't need wine on Windows."
+        ;;
+    OSX*)
+        # We use Wine 4 because Wine 5 uses 5x more disk space than Wine 4
+        export WINE_USR_DIR="${HAM_TOOLSET_DIR}/osx-x64/wine_4/usr"
+        if [ ! -e "$WINE_USR_DIR/bin/wine64" ]; then
+            toolset_check_and_dl_ver wine osx-x64 v4|| return 1
+            echo "I/Making sure that Wine isn't quarantined..."
+            sudo xattr -r -d com.apple.quarantine "${WINE_USR_DIR}/bin"
+            sudo xattr -r -d com.apple.quarantine "${WINE_USR_DIR}/lib"
+            sudo xattr -r -d com.apple.quarantine "${WINE_USR_DIR}/lib64"
+        fi
+        export WINE_PREFIX_DIR="${HAM_TOOLSET_DIR}/osx-x64/wine-prefix"
+        export WINEPREFIX="$WINE_PREFIX_DIR"
+        export WINEARCH=win64
+        export LC_ALL=en_EN.UTF-8
+        # export WINEDEBUG=-all
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
+
+# path
+export PATH="${HAM_TOOLSET_DIR}":${PATH}
+
+# version
+VER="--- wine ------------------------------
+Wine: $WINE_USR_DIR"
+if [ $? != 0 ]; then
+    echo "E/Can't get nicgc version."
+    return 1
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
