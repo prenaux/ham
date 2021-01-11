8ddb878fe7e4598b053523cfde9095089e921210
Arantir <zx.arantir@gmail.com>
2021-01-11 11:48:45 +0800

    Fixed macos warning_for_no_symbols problem

--- toolsets/cmake/toolchains/x86_64-msys.cmake
+++ toolsets/cmake/toolchains/x86_64-msys.cmake
@@ -1,6 +1,9 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
+set(CMAKE_C_COMPILER_WORKS TRUE)
+set(CMAKE_CXX_COMPILER_WORKS TRUE)
+
 if (WIN32)
   set(CLANG_BASE_PATH "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/Llvm/bin")
   set(CLANG_EXEC_EXT ".exe")
@@ -8,7 +11,7 @@ elseif (APPLE)
   set(CLANG_BASE_PATH "/usr/local/Cellar/llvm/11.0.0/bin")
 endif()
 
-set(CMAKE_RC_COMPILER "${CLANG_BASE_PATH}/clang${CLANG_EXEC_EXT}")
+set(CMAKE_RC_COMPILER "${CLANG_BASE_PATH}/llvm-rc${CLANG_EXEC_EXT}")
 set(CMAKE_C_COMPILER "${CLANG_BASE_PATH}/clang${CLANG_EXEC_EXT}")
 set(CMAKE_CXX_COMPILER "${CLANG_BASE_PATH}/clang++${CLANG_EXEC_EXT}")
 
@@ -16,7 +19,7 @@ set(triple x86_64-pc-win32)
 set(CMAKE_C_COMPILER_TARGET ${triple})
 set(CMAKE_CXX_COMPILER_TARGET ${triple})
 
-set(TOOLSET_BASE "/Users/Arantir/Work/ham/toolsets/msvc_19_x64/nt-x86/")
+set(TOOLSET_BASE "$ENV{HAM_HOME}/toolsets/msvc_19_x64/nt-x86/")
 set(MSVC_BASE "${TOOLSET_BASE}/2019/BuildTools/VC/Tools/MSVC/14.27.29110")
 set(WIN_SDK_BASE "${TOOLSET_BASE}/winsdk/10")
 set(WIN_SDK_VERSION "10.0.18362.0")
