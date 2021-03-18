27a205649f4f6f2c78dbb8ce75be28ca8a3f1d1e
Pierre Renaux <pierre@talansoft.com>
2021-03-19 01:11:37 +0800

    osx-arm64: Added genuuid & hamgrep for macOS arm64 ;

--- sources/emacs/site/ni-user-pierre.el
+++ sources/emacs/site/ni-user-pierre.el
@@ -254,7 +254,9 @@ If the new path's directories does not exist, create them."
 
  ;;; Use hamgrep on macOS
  (OSX
-  (setq pt-executable (concat "\"" HAM_HOME "/bin/osx-x64/hamgrep"  "\""))
+  (if (string-match "^aarch64-.*" system-configuration)
+      (setq pt-executable (concat "\"" HAM_HOME "/bin/osx-arm64/hamgrep"  "\""))
+    (setq pt-executable (concat "\"" HAM_HOME "/bin/osx-x64/hamgrep"  "\"")))
   (setq pt-arguments (list "--smart-case")))
 
 )
