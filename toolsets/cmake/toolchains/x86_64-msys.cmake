3f23ef06143385142393719aa55938a6fc4ce957
Arantir <zx.arantir@gmail.com>
2021-04-12 15:14:42 +0800

    cmake: Fixed clang building issues

--- toolsets/cmake/toolchains/x86_64-msys.cmake
+++ toolsets/cmake/toolchains/x86_64-msys.cmake
@@ -1,8 +1,9 @@
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
-
 set(CMAKE_C_COMPILER_WORKS TRUE)
 set(CMAKE_CXX_COMPILER_WORKS TRUE)
+
+include($ENV{HAM_TOOLSET_DIR}/tools.cmake)
 
 if (WIN32)
   set(CLANG_BASE_PATH "C:/Program Files (x86)/Microsoft Visual Studio/2019/Community/VC/Tools/Llvm/bin")
@@ -30,7 +31,7 @@ link_directories("${MSVC_BASE}/lib/x64")
 link_directories("${WIN_SDK_LIB}/ucrt/x64")
 link_directories("${WIN_SDK_LIB}/um/x64")
 
-include_directories(niLang SYSTEM
+include_directories(BEFORE SYSTEM
   "${MSVC_BASE}/include"
   "${WIN_SDK_INCLUDE}/ucrt"
   "${WIN_SDK_INCLUDE}/um"
@@ -38,3 +39,5 @@ include_directories(niLang SYSTEM
   "${WIN_SDK_INCLUDE}/winrt"
   "${WIN_SDK_INCLUDE}/cppwinrt"
   )
+
+add_flags("-m64" "-DWIN32")
