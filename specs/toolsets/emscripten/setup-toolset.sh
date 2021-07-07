f9e0bd1a0d8a92b8065463b9b4db24be5d566d96
Pierre Renaux <pierre@talansoft.com>
2021-07-07 18:01:36 +0800

    toolsets/emscripten: Updated to v2.0.25 for macOS, removed the now deprecated and untested Windows version ;

--- specs/toolsets/emscripten/setup-toolset.sh
+++ specs/toolsets/emscripten/setup-toolset.sh
@@ -1,69 +1,33 @@
 #!/bin/bash
 
-toolset_import python_27
+toolset_import python_36
 if [ $? != 0 ]; then return 1; fi
 toolset_import nodejs
 if [ $? != 0 ]; then return 1; fi
 toolset_import xslt_tools
 if [ $? != 0 ]; then return 1; fi
-toolset_import cmake
-if [ $? != 0 ]; then return 1; fi
 
 # toolset
 export HAM_TOOLSET=EMSCRIPTEN
-export HAM_TOOLSET_VER=1_25
+export HAM_TOOLSET_VER=2_0
 export HAM_TOOLSET_NAME=emscripten
 export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/emscripten"
 
-# Make the JSCC temporary folder
-mkdir -p "$HOME/_ham/jscc/"
-
-export EMSCRIPTEN_ROOT="${WORK}/emscripten"
-export EMSCRIPTEN="${EMSCRIPTEN_ROOT}"
-export NODE_JS="node"
-export PYTHON="python"
-export PATH="${EMSCRIPTEN_ROOT}":$PATH
-
-# When not using a FASTCOMP enabled clang
-# export EMCC_FAST_COMPILER=0
-
 # path setup
 case $HAM_OS in
-    NT*)
-        export LLVM_ROOT="${HAM_TOOLSET_DIR}/nt-x86/clang"
-        if [ ! -e "$LLVM_ROOT/clang.exe" ]; then
-            toolset_dl emscripten emscripten_nt-x86
-            if [ ! -e "$LLVM_ROOT/clang.exe" ]; then
-                echo "E/nt-x86 folder doesn't exist in the toolset"
-                return 1
-            fi
-        fi
-        export PATH="${LLVM_ROOT}":${PATH}
-        ;;
     OSX*)
-        export LLVM_ROOT="${HAM_TOOLSET_DIR}/osx-x64/clang/e1.25.0_64bit/"
-        export TEMP="$HOME/_ham/emscripten/"
-        mkdir -p $TEMP
+        if [ -z `which emcc` ]; then
+            echo "W/Couldn't find emcc, will try to install it with brew."
+            xcode-select --install
+            brew install emscripten
+        fi
+        export PATH=${HAM_TOOLSET_DIR}:${PATH}
         ;;
     *)
         echo "E/Toolset: Unsupported host OS"
         return 1
         ;;
 esac
-
-if [ ! -e "$EMSCRIPTEN_ROOT/emcc" ]; then
-    echo "Clone the emscripten repo into your WORK folder. (git clone https://github.com/prenaux/emscripten.git \"$WORK/emscripten\")"
-    return 1
-fi
-
-EMSCRIPTEN_DEFAULT_DOT_FILE="${HAM_TOOLSET_DIR}/dot_emscripten"
-
-# copy a configured .emscripten if needed
-if [ ! -f "$HOME/.emscripten" -o ! -f "$HOME/.emscripten_sanity" ]; then
-    echo "# Copying default .emscripten"
-    cp -f "$EMSCRIPTEN_DEFAULT_DOT_FILE" "$HOME/.emscripten"
-    emcc --version
-fi
 
 VER="--- emscripten ------------------------
 `emcc --version`"
