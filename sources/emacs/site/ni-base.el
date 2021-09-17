0693e860035ff8f216b062b74ffcfe30d4d8658a
Pierre Renaux <pierre@talansoft.com>
2021-09-18 00:46:58 +0800

    emacs/site: Added Emacs 28 compatibility ;

--- sources/emacs/site/ni-base.el
+++ sources/emacs/site/ni-base.el
@@ -1,4 +1,5 @@
 (provide 'ni-base)
+(require 'ni-emacs-compat)
 
 (defun agl-getenv (str)
   (if (getenv str)
@@ -55,122 +56,6 @@
    ((atom mylist) (list mylist))
    (t
     (append (agl-list-flatten (car mylist)) (agl-list-flatten (cdr mylist))))))
-
-;;;======================================================================
-;;; Versions
-;;;======================================================================
-(defmacro Aquamacs (&rest x)
-  (list
-   'if
-   (string-match "Aquamacs" (prin1-to-string (version)))
-   (cons 'progn x)))
-(defmacro GNUEmacs (&rest x)
-  (list
-   'if
-   (string-match "GNU Emacs" (prin1-to-string (version)))
-   (cons 'progn x)))
-(defmacro GNUEmacs20 (&rest x)
-  (list
-   'if
-   (string-match "GNU Emacs 20" (prin1-to-string (version)))
-   (cons 'progn x)))
-(defmacro GNUEmacs21 (&rest x)
-  (list
-   'if
-   (string-match "GNU Emacs 21" (prin1-to-string (version)))
-   (cons 'progn x)))
-(defmacro GNUEmacs22 (&rest x)
-  (list
-   'if
-   (string-match "GNU Emacs 22" (prin1-to-string (version)))
-   (cons 'progn x)))
-(defmacro GNUEmacs23 (&rest x)
-  (list
-   'if
-   (string-match "GNU Emacs 23" (prin1-to-string (version)))
-   (cons 'progn x)))
-(defmacro GNUEmacs24 (&rest x)
-  (list
-   'if
-   (string-match "GNU Emacs 24" (prin1-to-string (version)))
-   (cons 'progn x)))
-(defmacro GNUEmacs25 (&rest x)
-  (list
-   'if
-   (string-match "GNU Emacs 25" (prin1-to-string (version)))
-   (cons 'progn x)))
-(defmacro GNUEmacs26 (&rest x)
-  (list
-   'if
-   (string-match "GNU Emacs 26" (prin1-to-string (version)))
-   (cons 'progn x)))
-(defmacro XEmacs (&rest x)
-  (list
-   'if
-   (string-match "XEmacs 21" (prin1-to-string (version)))
-   (cons 'progn x)))
-(defmacro Linux (&rest x)
-  (list
-   'if
-   (string-match "linux" (prin1-to-string system-type))
-   (cons 'progn x)))
-(defmacro OSX (&rest x)
-  (list
-   'if
-   (string-match "darwin" (prin1-to-string system-type))
-   (cons 'progn x)))
-(defmacro Windows (&rest x)
-  (list
-   'if
-   (string-match "windows" (prin1-to-string system-type))
-   (cons 'progn x)))
-(defmacro NotWindows (&rest x)
-  (list
-   'if
-   (not (string-match "windows" (prin1-to-string system-type)))
-   (cons 'progn x)))
-(defmacro Solaris (&rest x)
-  (list
-   'if
-   (string-match "usg-unix-v" (prin1-to-string system-type))
-   (cons 'progn x)))
-(defmacro XWindow (&rest x)
-  (list
-   'if
-   (string-match "x" (prin1-to-string window-system))
-   (cons 'progn x)))
-(defmacro BatchMode (&rest x)
-  (list
-   'if
-   (and ENV_EMACS_BATCHMODE (string-match "1" ENV_EMACS_BATCHMODE))
-   (cons 'progn x)))
-(defmacro NotBatchMode (&rest x)
-  (list
-   'if
-   (not (and ENV_EMACS_BATCHMODE (string-match "1" ENV_EMACS_BATCHMODE)))
-   (cons 'progn x)))
-
-(defmacro IsTerminal (&rest x)
-  (list
-   'if
-   (string-match "nil" (prin1-to-string window-system))
-   (cons 'progn x)))
-
-(defmacro IsNotTerminal (&rest x)
-  (list
-   'if
-   (not (string-match "nil" (prin1-to-string window-system)))
-   (cons 'progn x)))
-
-(defmacro DontExecute (&rest x) ())
-
-(defun agl-begin-time-block (name)
-  (setq *emacs-time-block-prev-begin* *emacs-time-block-begin*)
-  (setq *emacs-time-block-begin* (float-time))
-  (NotBatchMode
-   (message (concat "[%3.3fs] [%3.3fs] === " name " ===")
-            (- (float-time) *emacs-time-start*)
-            (- (float-time) *emacs-time-block-prev-begin*))))
 
 ;;;======================================================================
 ;;; OS Environment
