a5fc7bfd809df69de67165de3e74efedcd576d5b
Pierre Renaux <pierre@talansoft.com>
2021-09-21 04:35:30 +0800

    bin2h: Output _RSTR variables as C++ multiline string ; Fixed never ending loop when reading using some text file as input ;

--- sources/bin2h/bin2h.c
+++ sources/bin2h/bin2h.c
@@ -7,6 +7,9 @@
 #  define _MAX_PATH (PATH_MAX-1)
 #endif
 
+#define BIN2H_VER "v2"
+#define BUF_SIZE 1024
+
 static const int NUM_COLS = 16;
 
 int main(int argc, char** argv) {
@@ -17,7 +20,9 @@ int main(int argc, char** argv) {
   char outputPath[_MAX_PATH];
 
   if (argc < 3) {
-    printf("BIN2H: input output (variable_name)\n");
+    printf("BIN2H (" BIN2H_VER "): input output (variable_name)\n");
+    printf("  If variable_name ends with _RSTR a C++ multiline string is output.\n");
+    printf("\n");
     printf("error: Invalid number of parameters !\n");
     goto _error;
   }
@@ -49,23 +54,49 @@ int main(int argc, char** argv) {
     varName = argv[3];
   }
 
-  {
+  int isRawString = 0;
+  if (strstr(varName,"_RSTR") != NULL) {
+    isRawString = 1;
+  }
+
+  if (isRawString) {
+    char buf[BUF_SIZE];
+    int size = 0;
+    fprintf(fpOut, "const char* %s = R\"\"\"(\n", varName);
+    while (1) {
+      int n = fread(buf,1,BUF_SIZE,fpIn);
+      if (n > 0) {
+        fwrite(buf,1,n,fpOut);
+        size += n;
+      }
+      if (n != BUF_SIZE) {
+        break;
+      }
+    }
+    fprintf(fpOut, "\n)\"\"\";\n");
+    fprintf(fpOut, "int %s_LEN = %d;\n", varName, size);
+  }
+  else {
+    unsigned char buf[BUF_SIZE];
     int size = 0;
     fprintf(fpOut, "unsigned char %s_DATA[] = {", varName);
     while (1) {
-      int byte = fgetc(fpIn);
-      if (feof(fpIn))
+      int n = fread(buf,1,BUF_SIZE,fpIn);
+      for (int i = 0; i < n; ++i) {
+        unsigned char byte = buf[i];
+        if ((size % NUM_COLS) == 0) {
+          fprintf(fpOut, "\n ");
+        }
+        if (size == 0) {
+          fprintf(fpOut, "0x%02x", byte);
+        }
+        else {
+          fprintf(fpOut, ",0x%02x", byte);
+        }
+        ++size;
+      }
+      if (n != BUF_SIZE)
         break;
-      if ((size % NUM_COLS) == 0) {
-        fprintf(fpOut, "\n ");
-      }
-      if (size == 0) {
-        fprintf(fpOut, "0x%02x", byte);
-      }
-      else {
-        fprintf(fpOut, ",0x%02x", byte);
-      }
-      ++size;
     }
     fprintf(fpOut, "\n};\n");
     fprintf(fpOut, "int %s_DATA_SIZE = %d;\n", varName, size);
