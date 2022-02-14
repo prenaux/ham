d8c2014252d1c482086f64f373d89cd42d44cf6e
Pierre Renaux <pierre@talansoft.com>
2022-02-14 20:06:52 +0800

    emacs/site/aglemacs.el: Better auto-revert ;

--- sources/emacs/site/aglemacs.el
+++ sources/emacs/site/aglemacs.el
@@ -687,8 +687,6 @@ BEG and END (region to sort)."
 (NotBatchMode
  (agl-begin-time-block "Basic stuffs")
 
- (global-auto-revert-mode 1)
-
  (setq-default fill-column 78)
 
  (put 'erase-buffer 'disabled nil)
@@ -766,6 +764,52 @@ BEG and END (region to sort)."
                           (concat "{" dired-directory "}"))
                          (t
                           "[no file]")))))
+)
+
+;;;======================================================================
+;;; Auto-revert
+;;;======================================================================
+(NotBatchMode
+
+ (setq auto-revert-interval 1)
+ (global-auto-revert-mode 1)
+
+ ;; Revert without prompt
+ ;; (setq revert-without-query '(".*"))
+
+ ;; Also auto refresh dired, but be quiet about it
+ ;; (setq global-auto-revert-non-file-buffers t)
+ ;; (setq auto-revert-verbose nil)
+
+ ;; Try to ignore modification-time-only changes in files, i.e. ones that
+ ;; don't really change the contents. This happens often with switching
+ ;; between different VC buffers.
+ (defun update-buffer-modtime-if-byte-identical ()
+   (let* ((size      (buffer-size))
+          (byte-size (position-bytes size))
+          (filename  buffer-file-name))
+     (when (and byte-size (<= size 1000000))
+       (let* ((attributes (file-attributes filename))
+              (file-size  (nth 7 attributes)))
+         (when (and file-size
+                    (= file-size byte-size)
+                    (string= (buffer-substring-no-properties 1 (1+ size))
+                             (with-temp-buffer
+                               (insert-file-contents filename)
+                               (buffer-string))))
+           (set-visited-file-modtime (nth 5 attributes))
+           t)))))
+
+ (defun verify-visited-file-modtime--ignore-byte-identical (original &optional buffer)
+   (or (funcall original buffer)
+       (with-current-buffer buffer
+         (update-buffer-modtime-if-byte-identical))))
+ (advice-add 'verify-visited-file-modtime :around #'verify-visited-file-modtime--ignore-byte-identical)
+
+ (defun ask-user-about-supersession-threat--ignore-byte-identical (original &rest arguments)
+   (unless (update-buffer-modtime-if-byte-identical)
+     (apply original arguments)))
+ (advice-add 'ask-user-about-supersession-threat :around #'ask-user-about-supersession-threat--ignore-byte-identical)
 )
 
 ;;;======================================================================
