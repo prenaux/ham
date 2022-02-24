2a3685e099913f178796948ea7be572e4b2dfba4
Pierre Renaux <pierre@talansoft.com>
2022-02-24 17:46:22 +0800

    Added a separated toolsets/svn ;

---
+++ specs/toolsets/svn/setup-toolset.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# toolset
+export HAM_TOOLSET=SVN
+export HAM_TOOLSET_NAME=svn
+export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/svn"
+
+export PATH=${HAM_TOOLSET_DIR}:${PATH}
+
+# platform
+case $HAM_OS in
+    OSX*)
+        ham-brew-install svn "bin/svn"
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
+
+# version check
+VER="--- svn --------------------------
+`svn --version | grep ", version"`"
+if [ $? != 0 ]; then
+    echo "E/Can't get SVN version."
+    return 1
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
