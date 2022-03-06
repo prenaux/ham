e77d0c0bf686f350eea3a6d440eccfaec6c3c51f
Pierre Renaux <pierre@talansoft.com>
2022-03-07 01:44:04 +0800

    bin/simple-http-server.py: Renamed wasm mode to crossOriginIsolation mode ; Added an encodings_map to emit the Content-Encoding when needed ; Handle additional file types used be wasm apps ;

--- bin/simple-http-server.py
+++ bin/simple-http-server.py
@@ -12,15 +12,15 @@ def printHelpAndExit():
     print("  simple-http-server mode dir")
     print("Modes:")
     print("  - regular: Simple standard HTTP server.")
-    print("  - wasm: HTTP server with cross origin isolation.\n" +
-          "          It can only load files under the same domain.")
+    print("  - crossOriginIsolation: HTTP server with cross origin isolation.\n" +
+          "                          It can only load files under the same domain.")
     exit(1)
 
 if len(sys.argv) > 1:
     mode = sys.argv[1]
     if (mode == "regular"):
         crossOriginIsolation = False
-    elif (mode == "wasm"):
+    elif (mode == "crossOriginIsolation"):
         crossOriginIsolation = True
     else:
         print("E/Unknown server mode specified: " + mode)
@@ -41,8 +41,23 @@ else:
     print("E/Missing parameter.")
     printHelpAndExit()
 
+encodings_map = {
+    ".jsgz": "gzip",
+    ".wasmgz": "gzip",
+    ".symbolsgz": "gzip",
+    ".memgz": "gzip",
+    ".datagz": "gzip",
+}
+
 class RequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
+        path = self.translate_path(self.path)
+        extension = os.path.splitext(path)[1]
+
+        if extension in encodings_map:
+            encoding = encodings_map[extension]
+            self.send_header("Content-Encoding", encoding)
+
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', '*')
         self.send_header('Access-Control-Allow-Headers', '*')
@@ -55,6 +70,14 @@ class RequestHandler(SimpleHTTPRequestHandler):
 # version, add in the Media Type.
 if sys.version_info < (3, 7, 5):
     RequestHandler.extensions_map['.wasm'] = 'application/wasm'
+
+RequestHandler.extensions_map['.jsgz'] = 'application/x-javascript'
+RequestHandler.extensions_map['.wasmgz'] = 'application/wasm'
+RequestHandler.extensions_map['.symbolsgz'] = 'text/plain'
+RequestHandler.extensions_map['.memgz'] = 'application/octet-stream'
+# NOTE: .data is NOT compressed
+RequestHandler.extensions_map['.data'] = 'application/octet-stream'
+RequestHandler.extensions_map['.datagz'] = 'application/octet-stream'
 
 if __name__ == '__main__':
     PORT = 8123
