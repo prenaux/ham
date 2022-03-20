ee5321ae1e8d1d68835e94beff8fa62dcadb58ee
Pierre Renaux <pierre@talansoft.com>
2022-03-20 19:25:06 +0800

    ham/toolsets/secrets_doppler: Added doppler setup for macOS ;

---
+++ specs/toolsets/secrets_doppler/setup-toolset.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# toolset
+export HAM_TOOLSET=secrets_doppler
+export HAM_TOOLSET_NAME=secrets_doppler
+export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/secrets_doppler"
+
+# path setup
+case $HAM_OS in
+    OSX*)
+        ham-brew-install gnupg "bin/gpg" || return 1
+        if [ -z "`which doppler`" ]; then
+            (set -ex; curl -Ls --tlsv1.2 --proto "=https" --retry 3 https://cli.doppler.com/install.sh | sudo sh)
+            if [ -z "`which doppler`" ]; then
+                echo "E/Can't install doppler."
+                return 1
+            fi
+        fi
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
+
+VER="--- secrets_doppler ------------
+`doppler --version`"
+if [ $? != 0 ]; then
+    echo "E/Can't get version."
+    return 1
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
