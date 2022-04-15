c9be7ee50fb114dd24ea6cf41429e75e3710a5f1
Pierre Renaux <pierre@talansoft.com>
2022-04-15 18:06:56 +0800

    toolsets/nodejs: macOS: Install the same way as on linux in a local folder instead of relying on brew ;

--- specs/toolsets/nodejs/setup-toolset.sh
+++ specs/toolsets/nodejs/setup-toolset.sh
@@ -18,42 +18,41 @@ case $HAM_OS in
         export PATH=${HAM_TOOLSET_DIR}:"${NODEJS_DIR}":"${NODEJS_DIR}/bin":${PATH}
         export NODE_PATH="$NODEJS_DIR/lib/node_modules"
         ;;
-    OSX*)
-        export NODEJS_HOME=`ham-brew-installdir node@16`
-        export PATH="${HAM_TOOLSET_DIR}":"${NODEJS_HOME}/bin":${PATH}
-        ham-brew-install node@16 "bin/node" || return 1
-        if [ -z `which node` -o -z `which npm` ]; then
-            # reinstall and link because we likely have a botched install
-            echo "W/Couldn't find node or npm, will try to reinstall it with brew."
-            ham-brew reinstall node@16
-            ham-brew link --overwrite node@16
-        fi
-        # TODO: Maybe this should just be $NODEJS_HOME/lib/node_modules.
-        #       That'll be needed if we need to use different nodejs versions
-        #       at once.
-        export NODEJS_GLOBAL_MODULES_DIR="/usr/local/lib/node_modules"
-        if [ ! -d "$NODEJS_GLOBAL_MODULES_DIR" ]; then
-            export NODEJS_GLOBAL_MODULES_DIR="/opt/homebrew/lib/node_modules"
-        fi
-        export NODE_PATH=$NODEJS_GLOBAL_MODULES_DIR
-        ;;
-    LINUX*)
+    OSX*|LINUX*)
         # We use a tag file to mark the version so that upgrades are automatically handled when we update the version number
         NODEJS_DL_VER=v16.14.2
         NODEJS_DL_TAG=$( echo ${NODEJS_DL_VER} | tr '.' '_' )
         export NODEJS_DIR="${HAM_TOOLSET_DIR}/${HAM_BIN_LOA}"
         export NODEJS_GLOBAL_MODULES_DIR="${NODEJS_DIR}/lib/node_modules"
-        export PATH=${HAM_TOOLSET_DIR}:"${NODEJS_DIR}/bin":"${NODEJS_DIR}/lib":${PATH}
+        export PATH=${HAM_TOOLSET_DIR}:"${NODEJS_DIR}/bin":${PATH}
         if [ ! -f "$NODEJS_DIR/$NODEJS_DL_TAG" ]; then
             echo "W/Couldn't find node, will try to from nodejs.org dist package."
-            NODEJS_DL_FN="node-${NODEJS_DL_VER}-linux-x64.tar.xz"
+            case "$HAM_BIN_LOA" in
+                osx-x64)
+                    NODEJS_DL_NAME="node-${NODEJS_DL_VER}-darwin-x64"
+                    NODEJS_DL_FN="${NODEJS_DL_NAME}.tar.gz"
+                    ;;
+                osx-arm64)
+                    NODEJS_DL_NAME="node-${NODEJS_DL_VER}-darwin-arm64"
+                    NODEJS_DL_FN="${NODEJS_DL_NAME}.tar.gz"
+                    ;;
+                lin-x64)
+                    NODEJS_DL_NAME="node-${NODEJS_DL_VER}-linux-x64"
+                    NODEJS_DL_FN="${NODEJS_DL_NAME}.tar.xz"
+                    ;;
+                *)
+                    complain nodejs_toolset "Unsupported arch '$HAM_BIN_LOA'."
+                    return 1;
+                    ;;
+            esac
             NODEJS_DL_URL="https://nodejs.org/dist/${NODEJS_DL_VER}/${NODEJS_DL_FN}"
+            NODEJS_DL_DEST="${HAM_HOME}/toolsets/_dl/${NODEJS_DL_FN}"
             (set -ex ;
              rm -Rf "$NODEJS_DIR" ;
-             curl "$NODEJS_DL_URL" -o ${NODEJS_DL_FN} ;
-             tar xf ${NODEJS_DL_FN} -C "${HAM_TOOLSET_DIR}" ;
-             mv "${HAM_TOOLSET_DIR}/node-${NODEJS_DL_VER}-linux-x64" "${NODEJS_DIR}" ;
-             rm ${NODEJS_DL_FN} )
+             curl "$NODEJS_DL_URL" -o "${NODEJS_DL_DEST}" ;
+             tar xf "${NODEJS_DL_DEST}" -C "${HAM_TOOLSET_DIR}" ;
+             mv "${HAM_TOOLSET_DIR}/${NODEJS_DL_NAME}" "${NODEJS_DIR}" ;
+             rm "${NODEJS_DL_DEST}" )
             if [ ! -f "$NODEJS_DIR/bin/node" ]; then
                 echo "E/'node' not found after unpacking the archive '%{NODEJS_DL_URL}'"
                 return 1
