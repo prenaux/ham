8ec4a18e022e82cd5d02e9f8f8d9df13f6887e94
Pierre Renaux <pierre@talansoft.com>
2022-04-21 20:05:50 +0800

    toolsets/python_36: Cleaner setup ; Added py3-venv to simplify greatly managing a python3 virtualenv ;

--- specs/toolsets/python_36/setup-toolset.sh
+++ specs/toolsets/python_36/setup-toolset.sh
@@ -27,33 +27,11 @@ case $HAM_OS in
         export PYTHON3_BINDIR="$HOME/AppData/Roaming/Python/Python36/Scripts/"
         pathenv_add "$PYTHON3_BINDIR"
         ;;
+
     OSX*)
-        if [ -z "which pip3" ]; then
-            echo "I/pip not found, installing..."
-            ham-brew install python3
-            errcheck $? python_36 "E/Can't install python 3"
-            # Install pytest so that we have a Library folder, its just
-            # simpler than trying to do without it
-            pip3 install --user pytest
-            errcheck $? python_36 "E/Can't install pytest with pip3"
-            if [ -z "which pip3" ]; then
-                errcheck $? python_36 "E/Can't install pip 3"
-            fi
-        fi
-        alias python=python3
-        if [ -e "$HOME/Library/Python/3.9/bin" ]; then
-            export PYTHON3_BINDIR=$HOME/Library/Python/3.9/bin
-        elif [ -e "$HOME/Library/Python/3.8/bin" ]; then
-            export PYTHON3_BINDIR=$HOME/Library/Python/3.8/bin
-        elif [ -e "$HOME/Library/Python/3.7/bin" ]; then
-            export PYTHON3_BINDIR=$HOME/Library/Python/3.7/bin
-        elif [ -e "$HOME/Library/Python/3.6/bin" ]; then
-            export PYTHON3_BINDIR=$HOME/Library/Python/3.6/bin
-        else
-            echo "E/Can't find a python library folder in $HOME/Library/Python/"
-            return 1
-        fi
-        pathenv_add "$PYTHON3_BINDIR"
+        ham-brew-install python3 "bin/python3" || return 1
+        export PYTHON3_HOME=`ham-brew-installdir python3`
+        pathenv_add "$PYTHON3_HOME/bin"
         ;;
 
     LINUX*)
@@ -65,6 +43,8 @@ case $HAM_OS in
         return 1
         ;;
 esac
+
+pathenv_add "$HAM_TOOLSET_DIR"
 
 VER="--- python3 --------------------------"
 if [ "$HAM_NO_VER_CHECK" != "1" ]; then
