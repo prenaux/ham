d97153c90c33734c9515e0a3460474154e9fab72
Arantir <zx.arantir@gmail.com>
2022-04-29 18:50:29 +0800

    Fixed some cmake build issues

---
+++ toolsets/cmake/toolchains/i686-msys.cmake
@@ -0,0 +1,39 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR i686-msys)
+set(CMAKE_C_COMPILER_WORKS TRUE)
+set(CMAKE_CXX_COMPILER_WORKS TRUE)
+
+if (APPLE)
+  set(CLANG_BASE_PATH "/usr/local/Cellar/llvm/11.0.0/bin")
+
+
+  set(CMAKE_RC_COMPILER "${CLANG_BASE_PATH}/llvm-rc${CLANG_EXEC_EXT}")
+  set(CMAKE_C_COMPILER "${CLANG_BASE_PATH}/clang${CLANG_EXEC_EXT}")
+  set(CMAKE_CXX_COMPILER "${CLANG_BASE_PATH}/clang++${CLANG_EXEC_EXT}")
+
+  set(triple x86_64-pc-win32)
+  set(CMAKE_C_COMPILER_TARGET ${triple})
+  set(CMAKE_CXX_COMPILER_TARGET ${triple})
+
+  set(TOOLSET_BASE "$ENV{HAM_HOME}/toolsets/msvc_19_x64/nt-x86/")
+  set(MSVC_BASE "${TOOLSET_BASE}/2019/BuildTools/VC/Tools/MSVC/14.27.29110")
+  set(WIN_SDK_BASE "${TOOLSET_BASE}/winsdk/10")
+  set(WIN_SDK_VERSION "10.0.18362.0")
+  set(WIN_SDK_INCLUDE ${WIN_SDK_BASE}/Include/${WIN_SDK_VERSION})
+  set(WIN_SDK_LIB ${WIN_SDK_BASE}/Lib/${WIN_SDK_VERSION})
+
+  link_directories("${MSVC_BASE}/lib/x64")
+  link_directories("${WIN_SDK_LIB}/ucrt/x64")
+  link_directories("${WIN_SDK_LIB}/um/x64")
+
+  include_directories(BEFORE SYSTEM
+    "${MSVC_BASE}/include"
+    "${WIN_SDK_INCLUDE}/ucrt"
+    "${WIN_SDK_INCLUDE}/um"
+    "${WIN_SDK_INCLUDE}/shared"
+    "${WIN_SDK_INCLUDE}/winrt"
+    "${WIN_SDK_INCLUDE}/cppwinrt"
+    )
+
+  add_flags("-m64" "-DWIN32")
+endif()
--- toolsets/cmake/toolchains/x86_64-darwin20.cmake
+++
@@ -1,5 +0,0 @@
-set(CMAKE_SYSTEM_NAME Darwin)
-set(CMAKE_SYSTEM_PROCESSOR x86_64)
-
-include($ENV{HAM_TOOLSET_DIR}/tools.cmake)
-add_flags("-m64" "-DOSX" "-mmacosx-version-min=10.13")
