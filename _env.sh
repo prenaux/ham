f1c32381e331122eb8b5c6bca5e23ef11934828b
Pierre Renaux <pierre@talansoft.com>
2022-05-07 16:04:29 +0800

    _env.sh: Added a basic environement that can be source to use ham ;

---
+++ _env.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# This is a simple environment setup to start using ham:
+#   source ./ham/_env.sh
+#
+
+# Don't `set -e` in a script that's sourced, otherwise will die as soon as any
+# command returns an error code.
+# set -e
+
+SCRIPT_SOURCED=$((return 0 2>/dev/null) && echo yes || echo "")
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+export HAM_HOME="$SCRIPT_DIR"
+export WORK="$( cd "$SCRIPT_DIR/.." && pwd )"
+
+if [ -z "$BASH_START_PATH" ]; then
+   export BASH_START_PATH="$PATH"
+fi
+
+if [ -z "$EDITOR" ]; then
+    export EDITOR=ham-editor
+fi
+
+# Set the ham environment
+. "$HAM_HOME/bin/ham-bash-setenv.sh"
+
+# Setup the tools that we want by default, just repos/git.
+. hat repos
+errcheck $? ham_env "Can't import toolsets." || return 1
