f1d6cd42b49892933017e5002e748f34cf1df002
Pierre Renaux <pierre@talansoft.com>
2022-05-07 15:58:56 +0800

    emacs/site/aglemacs.el: Added ni-sort-words & ni-sort-symbols utility functions ;

--- sources/emacs/site/aglemacs.el
+++ sources/emacs/site/aglemacs.el
@@ -1181,5 +1181,24 @@ This command does not push text to `kill-ring'."
      (beginning-of-line 1)
      (setq p2 (point))
      (delete-region p1 p2)))
-
 )
+
+;;;======================================================================
+;;; Sort words
+;;;======================================================================
+(defun ni-sort-symbols (reverse beg end)
+  "Sort symbols in region alphabetically, in REVERSE if negative.
+    See `sort-words'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\(\\sw\\|\\s_\\)+" "\\&" beg end))
+
+(defun ni-sort-words (reverse beg end)
+  "Sort words in region alphabetically, in REVERSE if negative.
+    Prefixed with negative \\[universal-argument], sorts in reverse.
+
+    The variable `sort-fold-case' determines whether alphabetic case
+    affects the sort order.
+
+    See `sort-regexp-fields'."
+  (interactive "*P\nr")
+  (sort-regexp-fields reverse "\\w+" "\\&" beg end))
