92de17f1fc28951a3b181c87c2f924031f8727b1
Pierre Renaux <pierre@talansoft.com>
2022-07-21 21:33:03 +0800

    toolsets/php: setup-toolset.sh: Added php7.4-pgsql & php7.4-sqlite3 ;

--- specs/toolsets/php/setup-toolset.sh
+++ specs/toolsets/php/setup-toolset.sh
@@ -32,7 +32,7 @@ case $HAM_OS in
     LINUX*)
         if [ -z `where_inpath php` ]; then
             echo "W/php not found, trying to install with sudo..."
-            sudo apt install -y php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-soap php7.4-zip php7.4-intl
+            sudo apt install -y php7.4-common php7.4-mysql php7.4-xml php7.4-xmlrpc php7.4-curl php7.4-gd php7.4-imagick php7.4-cli php7.4-dev php7.4-imap php7.4-mbstring php7.4-opcache php7.4-pgsql php7.4-soap php7.4-sqlite3 php7.4-zip php7.4-intl
         fi
         pathenv_add "${HAM_TOOLSET_DIR}"
         ;;
