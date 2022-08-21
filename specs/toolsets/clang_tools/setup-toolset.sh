8fdd4505c4e85f81392bc8e41c07731dc7f6a1b7
Pierre Renaux <pierre@talansoft.com>
2022-08-21 19:49:38 +0800

    toolsets/clang_tools: Added new clang_tools toolset for clang-tidy, clang-query, etc... ;

---
+++ specs/toolsets/clang_tools/setup-toolset.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# toolset
+export HAM_TOOLSET=clang_tools
+export HAM_TOOLSET_NAME=clang_tools
+export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/clang_tools"
+
+case "$HAM_BIN_LOA" in
+    osx-x64|osx-arm64)
+        ham-brew-install llvm "bin/clang-query"
+        pathenv_add "$(ham-brew-installdir llvm/bin)"
+        ;;
+    *)
+        complain clang_tools "Unsupported arch '$HAM_BIN_LOA'."
+        return 1;
+        ;;
+esac
+pathenv_add "$HAM_TOOLSET_DIR"
+
+VER="--- clang_tools ------------------------"
+if [ "$HAM_NO_VER_CHECK" != "1" ]; then
+    VER="$VER
+`clang-tidy --version | grep version`"
+    if [ $? != 0 ]; then
+        echo "E/Can't get version."
+        return 1
+    fi
+fi
+
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
