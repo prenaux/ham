1541d0f2adb6cc3ff912569fa3e0c7445293748a
Pierre Renaux <pierre@talansoft.com>
2022-08-31 22:14:30 +0800

    toolsets/linux_x64: Can use Clang to compile on linux with LINUX_CLANG=1 ;

--- specs/toolsets/linux_x64/setup-toolset.sh
+++ specs/toolsets/linux_x64/setup-toolset.sh
@@ -2,8 +2,6 @@
 
 . ham-toolset-import.sh xslt_tools
 if [ $? != 0 ]; then return 1; fi
-toolset_import gcc_470
-if [ $? != 0 ]; then return 1; fi
 
 case $HAM_OS in
     LINUX)
@@ -14,5 +12,36 @@ case $HAM_OS in
         ;;
 esac
 
+export LINUX_CLANG=${LINUX_CLANG:-1}
 export OSPLAT=X64
-export BUILD_BIN_LOA=lin-x64
+export BUILD_BIN_LOA=$HAM_BIN_LOA
+
+# Use GCC
+if [ "${LINUX_CLANG}" == "0" ]; then
+  toolset_import gcc_470
+  if [ $? != 0 ]; then return 1; fi
+
+  VER="--- linux_x64 -----------------------
+Using GCC"
+else
+  # Use clang
+  export HAM_TOOLSET=CLANG
+  export HAM_TOOLSET_NAME=clang_33
+  export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/${HAM_TOOLSET_NAME}"
+  export HAM_CPP_TOOLSET=$HAM_TOOLSET
+  export HAM_CPP_TOOLSET_NAME=$HAM_TOOLSET_NAME
+
+  # finding correct clang compiler dir
+  local dir=$(clang --version | grep InstalledDir)
+  export CMD_JSON_COMPILER_PATH=${dir#*' '}/
+
+  VER="--- linux_x64 -----------------------
+`clang -arch x86_64  --version`"
+  if [ $? != 0 ]; then
+    echo "E/Can't get version."
+    return 1
+  fi
+fi
+
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
