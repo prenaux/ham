5a56f382fc74e3a8dde1f26a0a6b399a66585c88
Pierre Renaux <pierre@talansoft.com>
2022-09-28 13:58:20 +0800

    Improve ham-cppm scripts ergonomics ;

--- specs/toolsets/thrift_cppm/setup-toolset.sh
+++ specs/toolsets/thrift_cppm/setup-toolset.sh
@@ -6,16 +6,16 @@ export HAM_TOOLSET_DIR="${HAM_HOME}/specs/toolsets/thrift_cppm"
 export HAM_HDRS_THRIFT_CPPM="${HAM_TOOLSET_DIR}/github-thrift/lib/cpp/src"
 
 CHECK_HEADER="${HAM_HDRS_THRIFT_CPPM}/thrift/TBase.h"
-CHECK_DLL="${HAM_HOME}/bin/${HAM_BIN_LOA}/`ham-cppm-dll-filename thrift_cppm_ra`"
+CHECK_DLL="${HAM_HOME}/bin/`ham-cppm-bin-filepath dll thrift_cppm_ra`"
 if [ ! -e "$CHECK_DLL" ] || [ ! -e "$CHECK_HEADER" ]; then
   ham-cppm-build thrift_cppm
   if [ ! -e "$CHECK_DLL" ]; then
     echo "E/Can't find build artifact '$CHECK_DLL'."
-    exit 1
+    return 1
   fi
   if [ ! -e "$CHECK_HEADER" ]; then
     echo "E/Can't find build artifact '$CHECK_HEADER'."
-    exit 1
+    return 1
   fi
 fi
 
