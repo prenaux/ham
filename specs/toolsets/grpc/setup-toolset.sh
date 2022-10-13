cb65679efadcd88dd9e6c45a720d049db1aa56ab
Pierre Renaux <pierre@talansoft.com>
2022-10-13 17:56:58 +0800

    toolsets/grpc/setup-toolset.sh: Add grpcwebproxy & grpcurl ; Remove envoy ; Use precompiled binaries which are much quicker to install ;

--- specs/toolsets/grpc/setup-toolset.sh
+++ specs/toolsets/grpc/setup-toolset.sh
@@ -5,88 +5,71 @@ export HAM_TOOLSET=GRPC
 export HAM_TOOLSET_NAME=grpc
 export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/grpc"
 
-# path setup
-case $HAM_OS in
-    OSX*)
-        # Separated for clarity and easier error tracking
-
-        # Protobuf, could move to it's own toolset.
-        # XXX: protobuf@3 because protobuf install a buffy version of protobuf that breaks protoc-gen-js
-        ham-brew-install protobuf@3 "bin/protoc" || return 1
-        PREFIX=`ham-brew-installdir protobuf@3 prefix`
-        export PROTOBUF_BINDIR="${PREFIX}/opt/protobuf"
-        export PROTOBUF_INCDIR="${PREFIX}/include/google/protobuf"
-        export PROTOBUF_LIBDIR="${PREFIX}/lib"
-
-        # Grpc
-        ham-brew-install grpc "bin/grpc_cli" || return 1
-        PREFIX=`ham-brew-installdir grpc prefix`
-        export GRPC_BINDIR="${PREFIX}/opt/grpc/bin"
-        # ideally we'd use ${PREFIX}/include/grpc but there's also grpc++ &
-        # grpcpp in there, its not clear which one we need and/or if we need
-        # all of them
-        export GRPC_INCDIR="${PREFIX}/include"
-        export GRPC_LIBDIR="${PREFIX}/lib"
-
-        # protoc-gen-grpc-web for client side Javascript
-        ham-brew-install protoc-gen-grpc-web "bin/protoc-gen-grpc-web" || return 1
-        export PROTOC_GEN_GRPC_WEB_EXE="`ham-brew-installdir protoc-gen-grpc-web bin`/bin/protoc-gen-grpc-web"
-
-        # envoy, to proxy http2 -> http1 for client side Javascript. Might be
-        # better as its own toolset but since the only know use case atm is
-        # for grpc its simpler to have it here.
-        ham-brew-install envoy "bin/envoy" || return 1
-        ;;
-    *)
-        echo "E/Toolset: Unsupported host OS"
-        return 1
-        ;;
+case $HAM_BIN_LOA in
+  osx-arm64)
+    # we only have a x64 exe atm
+    BIN_LOA=osx-x64
+    ;;
+  osx-x86|lin-x64|nt-x86)
+    BIN_LOA=$HAM_BIN_LOA
+    ;;
+  *)
+    echo "E/Toolset: Unsupported host HAM_BIN_LOA '$HAM_BIN_LOA'"
+    return 1
+    ;;
 esac
 
-pathenv_add "$HAM_TOOLSET_DIR"
+# check and dl toolset
+toolset_check_and_dl_ver grpc ${BIN_LOA} v22_10_13 || return 1
+
+# path
+export GRPC_DIST_DIR="${HAM_TOOLSET_DIR}/${BIN_LOA}"
+pathenv_add "${GRPC_DIST_DIR}"
+pathenv_add "${GRPC_DIST_DIR}/protobuf-javascript/bin"
+pathenv_add "${GRPC_DIST_DIR}/protoc/bin"
+pathenv_add "${HAM_TOOLSET_DIR}"
 
 VER="--- protobuf --------------------------"
 if [ "$HAM_NO_VER_CHECK" != "1" ]; then
-  ham-check-file "$PROTOBUF_INCDIR/message.h"
-  errcheck $? ${HAM_TOOLSET_NAME}_protobuf "Include check failed." || return 1
-  ham-check-file "$PROTOBUF_LIBDIR/libprotobuf.a"
-  errcheck $? ${HAM_TOOLSET_NAME}_protobuf "Lib check failed." || return 1
-
   VER="$VER
 `protoc --version`"
-  errcheck $? ${HAM_TOOLSET_NAME}_protobuf "Version check failed." || return 1
+  errcheck $? ${HAM_TOOLSET_NAME} "protobuf version check failed." || return 1
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
+
+VER="--- protoc-gen-js ---------------"
+if [ "$HAM_NO_VER_CHECK" != "1" ]; then
+  VER="$VER
+Location: `where_inpath protoc-gen-js`"
+  errcheck $? ${HAM_TOOLSET_NAME} "protoc-gen-js location check failed." || return 1
 fi
 export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
 $VER"
 
 VER="--- protoc-gen-grpc-web ---------------"
-if [ -z `where_inpath protoc-gen-grpc-web` ]; then
-  die ${HAM_TOOLSET_NAME}_protoc-gen-grpc-web "Exe check failed." || return 1
+if [ "$HAM_NO_VER_CHECK" != "1" ]; then
+  VER="$VER
+Location: `where_inpath protoc-gen-grpc-web`"
+  errcheck $? ${HAM_TOOLSET_NAME} "protoc-gen-grpc-web location check failed." || return 1
 fi
 export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
 $VER"
 
-VER="--- grpc ------------------------------"
+VER="--- grpcurl --------------------------"
 if [ "$HAM_NO_VER_CHECK" != "1" ]; then
-  ham-check-file "$GRPC_INCDIR/grpc/grpc.h"
-  errcheck $? $HAM_TOOLSET_NAME "Include check failed." || return 1
-  ham-check-file "$GRPC_LIBDIR/libgrpc.dylib"
-  errcheck $? $HAM_TOOLSET_NAME "Lib check failed." || return 1
-
-  # We check only for existence since grpc_cli doesnt have a version command :(
-  ham-check-file "$GRPC_BINDIR/grpc_cli"
-  errcheck $? $HAM_TOOLSET_NAME "Exe check failed." || return 1
   VER="$VER
-grpc_cli"
+`grpcurl --version 2>&1`"
+  errcheck $? ${HAM_TOOLSET_NAME} "grpcurl version check failed." || return 1
 fi
 export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
 $VER"
 
-VER="--- envoy -----------------------------"
+VER="--- grpcwebproxy -----------------------------"
 if [ "$HAM_NO_VER_CHECK" != "1" ]; then
   VER="$VER
-`envoy --version | grep version`"
-  errcheck $? ${HAM_TOOLSET_NAME}_envoy "Version check failed." || return 1
+Location: `where_inpath grpcwebproxy`"
+  errcheck $? ${HAM_TOOLSET_NAME}_grpcwebproxy "grpcwebproxy location check failed." || return 1
 fi
 export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
 $VER"
