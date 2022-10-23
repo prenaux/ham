3674b8cdb2f13bf0c5da50586b130895f26b3b00
Pierre Renaux <pierre@talansoft.com>
2022-10-23 16:29:16 +0800

    specs/toolsets/xslt_tools: Fix a different error code being returned with Java 11 leading to toolset import failure when checking the xslt saxon version ;

--- specs/toolsets/xslt_tools/setup-toolset.sh
+++ specs/toolsets/xslt_tools/setup-toolset.sh
@@ -44,15 +44,25 @@ fi
 export PATH="${HAM_TOOLSET_DIR}":${PATH}
 
 # version
-VER="--- xslt_tools_1 ----------------------"
+VER="--- xslt_tools ------------------------"
 if [ "$HAM_NO_VER_CHECK" != "1" ]; then
     VER="$VER
 --- xsltproc ---
-`xsltproc --version | grep xsltproc`
---- saxon ------
-`xslt_tools-saxon 2>&1 | grep Saxon`"
+`xsltproc --version | grep xsltproc`"
     if [ $? != 0 ]; then
         echo "E/Can't get xsltproc version."
+        return 1
+    fi
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
+
+if [ "$HAM_NO_VER_CHECK" != "1" ]; then
+    VER="$VER
+--- saxon ------
+`xslt_tools-saxon -version`"
+    if [ $? != 0 ]; then
+        echo "E/Can't get xslt_tools-saxon version."
         return 1
     fi
 fi
