40ab0f040b1f3e493a1d22df885ed48b923099df
Pierre Renaux <pierre@talansoft.com>
2022-11-07 01:16:58 +0800

    specs/toolsets/php_8: Add php_8 toolset ;

---
+++ specs/toolsets/php_8/setup-toolset.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# toolset
+export HAM_TOOLSET=PHP
+export HAM_TOOLSET_NAME=php_8
+export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/php_8"
+
+# path setup
+case $HAM_OS in
+    OSX*)
+        export PHP_HOME=`ham-brew-installdir php@8.0`
+        # freetds, a dependency of php@8.0, needs brew's curl and for some
+        # reason complains about it and fail the install instead of having it
+        # as a dependency...
+        ham-brew-install curl "bin/curl" || return 1
+        ham-brew-install php@8.0 "bin/php" || return 1
+        export PATH="${HAM_TOOLSET_DIR}":"${HOME}/.composer/vendor/bin":"${PHP_HOME}/bin":${PATH}
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
+
+VER="--- php_8 ----------------------"
+if [ "$HAM_NO_VER_CHECK" != "1" ]; then
+    VER="$VER
+`php --version`
+`composer -V`"
+    if [ $? != 0 ]; then
+      echo "E/Can't get version."
+      return 1
+    fi
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
