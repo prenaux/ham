6e29b95829a1776b8fa1d1f2e8bfb1ee6f26400e
Pierre <pierre@bytecollider.com>
2022-12-09 15:58:43 +0800

    Update toolsets/nginx to use ham-brew-install on macOS ;

--- specs/toolsets/nginx/setup-toolset.sh
+++ specs/toolsets/nginx/setup-toolset.sh
@@ -7,26 +7,8 @@ export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/nginx"
 
 # path setup
 case $HAM_OS in
-    NT*)
-        export NGINX_DIR="${HAM_TOOLSET_DIR}/nt-x86/"
-        export PATH="${NGINX_DIR}":"${HAM_TOOLSET_DIR}":${PATH}
-        if [ ! -e "$NGINX_DIR/nginx.exe" ]; then
-            toolset_dl nginx nginx_nt-x86
-            if [ ! -e "$NGINX_DIR/nginx.exe" ]; then
-                echo "E/nt-x86 folder doesn't exist in the toolset"
-                return 1
-            fi
-        fi
-        ;;
     OSX*)
-        if [ ! -x "$(command -v nginx)" ]; then
-            echo "I/nginx not found, using brew to install it..."
-            ham-brew install nginx
-        fi
-        if [ ! -x "$(command -v certbot)" ]; then
-            echo "I/certbox not found, using brew to install it..."
-            brew install certbot
-        fi
+        ham-brew-install nginx "bin/nginx"
         ;;
     *)
         echo "E/Toolset: Unsupported host OS"
