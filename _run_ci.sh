31983fb8a85761747b0f000901c28634629d4bb5
Pierre Renaux <pierre@talansoft.com>
2023-04-13 11:19:17 +0800

    _run_ci.sh: Add a cppm module to test the default C/C++ compiler beyond the absolute basics ;

--- _run_ci.sh
+++ _run_ci.sh
@@ -16,3 +16,22 @@ echo "I/Build and run pi with ham"
 (set -x;
  cd "$HAM_HOME/sources/ham/tests/pi" ;
  ham Run_pi)
+
+echo "I/Build cppm modules."
+case $HAM_OS in
+    NT*)
+        echo "W/thrift_cppm module not supported yet on Windows."
+        ;;
+    OSX*)
+        (set -x ;
+         ham-cppm-build thrift_cppm)
+        ;;
+    LINUX*)
+        (set -x ;
+         ham-cppm-build thrift_cppm)
+        ;;
+    *)
+        echo "E/Toolset: Unsupported host OS"
+        return 1
+        ;;
+esac
