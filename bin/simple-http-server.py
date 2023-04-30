a5a1cef6b877d16e4bf3a0bd352991d7d4960cb5
Pierre <pierre@bytecollider.com>
2023-04-30 21:56:38 +0800

    bin/simple-http-server.py: Add accessed files log file & option to add all of them to a zip file ;

--- bin/simple-http-server.py
+++ bin/simple-http-server.py
@@ -3,13 +3,18 @@ import sys
 import os
 import socketserver
 import argparse
+import logging
+import zipfile
 from http.server import SimpleHTTPRequestHandler
+from threading import Lock
 
 # Set up the argument parser
 parser = argparse.ArgumentParser(description='Run a static HTTP server.')
 parser.add_argument('mode', choices=['regular', 'crossOriginIsolation'], help='The mode for the server.')
 parser.add_argument('-d', '--dir', default='.', help='The directory to serve.')
 parser.add_argument('-p', '--port', type=int, default=8123, help='The port to listen on.')
+parser.add_argument('-l', '--log', default=None, help='The log file to store opened files.')
+parser.add_argument('-z', '--zip', default=None, help='The zip file to store accessed files.')
 
 # Parse the arguments
 args = parser.parse_args()
@@ -24,6 +29,23 @@ if (not os.path.isdir(dir)):
 print("I/Serving directory: " + os.path.abspath(dir))
 os.chdir(dir)
 
+log_file = args.log
+if log_file:
+    logging.basicConfig(level=logging.INFO, filename=log_file, filemode='a', format='%(asctime)s - %(message)s')
+    print("I/Logging to: " + log_file)
+else:
+    logging.basicConfig(level=logging.ERROR)
+
+accessed_files = set()
+accessed_files_lock = Lock()
+
+zip_file = args.zip
+if zip_file:
+    print("I/Zipping to: " + zip_file)
+
+if zip_file and os.path.exists(zip_file):
+    raise FileExistsError(f"The specified zip file '{zip_file}' already exists.")
+
 encodings_map = {
     ".jsgz": "gzip",
     ".wasmgz": "gzip",
@@ -32,10 +54,35 @@ encodings_map = {
     ".datagz": "gzip",
 }
 
+def handle_accessed_file(path):
+  if (not os.path.isfile(path)):
+    return
+
+  with accessed_files_lock:
+    rel_path = os.path.relpath(path, os.path.abspath(dir))
+    if rel_path in accessed_files:
+      return
+
+    accessed_files.add(rel_path)
+
+    # Log the requested file path
+    if log_file:
+      logging.info("Accessing:\n- rel: %s\n- abs: %s", rel_path, path)
+
+    # Append the accessed file to the specified zip file if it hasn't been zipped yet
+    if zip_file:
+      with zipfile.ZipFile(zip_file, 'a') as zf:
+        try:
+          zf.write(path, arcname=rel_path)
+        except Exception as e:
+          logging.error("Error appending file to zip: %s", e)
+
 class RequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         path = self.translate_path(self.path)
         extension = os.path.splitext(path)[1]
+
+        handle_accessed_file(path)
 
         if extension in encodings_map:
             encoding = encodings_map[extension]
@@ -47,6 +94,7 @@ class RequestHandler(SimpleHTTPRequestHandler):
         if mode == "crossOriginIsolation":
             self.send_header('Cross-Origin-Opener-Policy', 'same-origin')
             self.send_header('Cross-Origin-Embedder-Policy', 'require-corp')
+
         SimpleHTTPRequestHandler.end_headers(self)
 
 # Python 3.7.5 adds in the WebAssembly Media Type. If this is an older
