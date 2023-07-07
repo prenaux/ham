d9bc2957b7815055086c9e20843a684079bcfc24
jamesrdd <james.g@reddoor.digital>
2023-07-07 12:42:07 +0800

    ham-test-thrift: Add scripts and CI action to build and deploy docker image of calculator service ;

---
+++ sources/ham/tests/ham-test-thrift/_docker_incontainer_run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+if [ ! -e "$DEPLOY_EXE" ]; then
+    echo "E/DEPLOY_EXE not found '$DEPLOY_EXE'."
+    exit 1
+fi
+if [ -z "$PORT" ]; then
+    echo "E/PORT not set '$PORT'."
+    exit 1
+fi
+$DEPLOY_EXE -Dport=${PORT}
