b56095a18af949346b80ea1f1fd2ecebcdc23729
Pierre <pierre@bytecollider.com>
2023-07-16 08:24:57 +0000

    specs/toolsets/cppm: Add cppm toolset which setup the C++ toolset taking into account BUILD_TARGET if its set ;

---
+++ specs/toolsets/cppm/setup-toolset.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+BUILD_TARGET=${BUILD_TARGET:-default}
+CPP_TOOLSET=$(ham-cppm-get-toolset "${BUILD_TARGET}")
+echo "I/cppm: BUILD_TARGET=${BUILD_TARGET}, CPP_TOOLSET=${CPP_TOOLSET}"
+errcheck $? "cppm: Can't get C++ toolset for BUILD_TARGET '$BUILD_TARGET'." || return 1
+toolset_import_once ${CPP_TOOLSET} || return 1
