8eefffc227b0cee6b59432ff52078128ce269c21
Pierre <pierre@bytecollider.com>
2023-07-20 15:27:23 +0800

    toolsets/cppm & toolsets/thrift_cppm: Fix incorrect error reporting ;

--- specs/toolsets/cppm/setup-toolset.sh
+++ specs/toolsets/cppm/setup-toolset.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 BUILD_TARGET=${BUILD_TARGET:-default}
+
 CPP_TOOLSET=$(ham-cppm-get-toolset "${BUILD_TARGET}")
+errcheck $? toolset_cppm "cppm: Can't get C++ toolset for BUILD_TARGET '$BUILD_TARGET'." || return 1
 echo "I/cppm: BUILD_TARGET=${BUILD_TARGET}, CPP_TOOLSET=${CPP_TOOLSET}"
-errcheck $? "cppm: Can't get C++ toolset for BUILD_TARGET '$BUILD_TARGET'." || return 1
+
 toolset_import_once ${CPP_TOOLSET} || return 1
--- specs/toolsets/thrift_cppm/setup-toolset.sh
+++ specs/toolsets/thrift_cppm/setup-toolset.sh
@@ -8,7 +8,10 @@ export HAM_HDRS_THRIFT_CPPM="${HAM_TOOLSET_DIR}/github-thrift/lib/cpp/src"
 BUILD=ra ham-cppm-build-check thrift_cppm noheader thrift_cppm thrift_cppm || return 1
 BUILD=da ham-cppm-build-check thrift_cppm noheader thrift_cppm thrift_cppm || return 1
 
+HAM_THRIFTC_EXE_PATH=$(BUILD_TARGET=default ham-cppm-exe-path thrift_cppm ham_thriftc)
+
 VER="--- thrift_cppm ---------------------
-$(`ham-cppm-exe-path thrift_cppm ham_thriftc` --version)"
+$(${HAM_THRIFTC_EXE_PATH} --version)"
+errcheck $? toolset_thrift_cppm "Can't get ham_thriftc version." || return 1
 export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
 $VER"
