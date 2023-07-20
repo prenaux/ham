a255b2556e7c76769ba464edae7d0c4c740259dc
Pierre Renaux <pierre@talansoft.com>
2023-07-21 01:45:35 +0800

    bin/ham-shell.cmd: Download repos toolset if it isn't already ;

--- bin/ham-shell.cmd
+++ bin/ham-shell.cmd
@@ -1,5 +1,30 @@
 @echo off
+setlocal EnableDelayedExpansion
+
 set PATH="%~dp0";"%~dp0\nt-x86"
 REM !! DO NOT add quotes below !!
 set HAM_HOME=%~dp0\..
-bash --rcfile "%HAM_HOME%\bin\ham-bash-start.sh" -i %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+REM Check if bash.exe exists, and if not, call ham-toolset-dl-and-extract.cmd
+if not exist "%HAM_HOME%\toolsets\repos\nt-x86\git\bin\bash.exe" (
+    echo I/bash.exe not found downloading the repos toolset...
+    call "%HAM_HOME%\bin\ham-toolset-dl-and-extract.cmd" repos repos_nt-x86_v4
+    if errorlevel 1 (
+        echo E/Failed to download and extract the toolset.
+        echo Press any key to exit...
+        pause >nul
+        exit /b 1
+    )
+    echo I/repos toolset downloaded
+)
+
+REM Check again if bash.exe exists after running ham-toolset-dl-and-extract.cmd
+if not exist "%HAM_HOME%\toolsets\repos\nt-x86\git\bin\bash.exe" (
+    echo E/bash.exe not found. Cannot proceed.
+    echo Press any key to exit...
+    pause >nul
+    exit /b 1
+)
+
+REM Now run bash.exe with the specified parameters
+"%HAM_HOME%\toolsets\repos\nt-x86\git\bin\bash.exe" --rcfile "%HAM_HOME%\bin\ham-bash-start.sh" -i %*
---
+++ bin/ham-toolset-dl-and-extract.cmd
@@ -0,0 +1,74 @@
+@echo off
+REM Example usage:
+REM   ham-toolset-dl-and-extract.cmd repos repos_nt-x86_v4
+set PATH="%~dp0";"%~dp0\nt-x86"
+REM !! DO NOT add quotes below !!
+set HAM_HOME=%~dp0\..
+
+setlocal EnableDelayedExpansion
+
+if "%~1" == "" (
+    echo Missing first argument. Syntax: %~nx0 TOOLSET_NAME ARCHIVE_NAME
+    exit /b 1
+)
+
+if "%~2" == "" (
+    echo Missing second argument. Syntax: %~nx0 TOOLSET_NAME ARCHIVE_NAME
+    exit /b 1
+)
+
+set "CWD=%CD%"
+set "DL_DIR=%HAM_HOME%\toolsets\_dl"
+set "DIR=%HAM_HOME%\toolsets\%~1"
+set "ARCH_URL=https://cdn2.talansoft.com/ftp/toolsets/%~2.7z"
+REM set "ARCH_URL=http://localhost:8123/data/toolsets/%~2.7z"
+set "DLFILENAME=_%~2.7z"
+
+echo === Importing toolset '%~1' from %DIR%
+mkdir "%DL_DIR%" 2>nul
+mkdir "%DIR%" 2>nul
+pushd "%DIR%" || (
+    echo E/Can't cd to the toolset's directory '%DIR%'.
+    exit /b 1
+)
+
+if exist "%DL_DIR%\%~2.7z" (
+    echo I/Copying archive found at: %DL_DIR%\%~2.7z
+    copy /Y "%DL_DIR%\%~2.7z" ".\%DLFILENAME%"
+)
+
+if errorlevel 1 (
+    echo E/Can't cd to the toolset's directory '%DIR%'.
+    exit /b 1
+)
+
+if exist "%DLFILENAME%" (
+    echo I/Extracting %DLFILENAME%
+    7z x -y "%DLFILENAME%"
+    if errorlevel 1 (
+        echo E/Extraction failed !
+        del "%DLFILENAME%"
+        popd
+        exit /b 1
+    )
+    popd
+) else (
+    echo I/Trying download from %ARCH_URL%
+    curl -o "%DLFILENAME%" "%ARCH_URL%"
+    if errorlevel 1 (
+        echo E/Download failed !
+        popd
+        exit /b 1
+    )
+    echo I/Extracting %DLFILENAME%
+    7z x -y "%DLFILENAME%"
+    if errorlevel 1 (
+        echo E/Extraction failed !
+        del "%DLFILENAME%"
+        popd
+        exit /b 1
+    )
+    popd
+)
+
+echo I/Done, downloaded and extracted toolset '%~1'.
