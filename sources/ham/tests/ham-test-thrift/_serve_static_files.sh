7fbf104317a94d7cb6da38ce2d8a3f00cf7cfdb5
Pierre Renaux <pierre@talansoft.com>
2023-07-21 23:36:46 +0800

    sources/ham/tests/ham-test-thrift: Fix test not running on Windows ;

--- sources/ham/tests/ham-test-thrift/_serve_static_files.sh
+++ sources/ham/tests/ham-test-thrift/_serve_static_files.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+#!/bin/bash -e
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$SCRIPT_DIR"
+. "$HAM_HOME/bin/ham-bash-setenv.sh"
 export HAM_NO_VER_CHECK=1
 . hat > /dev/null
 set -ex
+
 simple-http-server regular
