6cc599514aaa821dae7b9f2818ef8759c7e9c833
Pierre <pierre@bytecollider.com>
2023-08-11 15:41:33 +0800

    toolsets/svn/setup-toolset.sh: Make sure git svn is available ;

--- specs/toolsets/svn/setup-toolset.sh
+++ specs/toolsets/svn/setup-toolset.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+toolset_import_once repos || return 1
 
 # toolset
 export HAM_TOOLSET=SVN
@@ -10,13 +11,18 @@ case $HAM_OS in
     NT*)
         toolset_check_and_dl_ver svn nt-x86 v1_14_1 || return 1
         pathenv_add "${HAM_TOOLSET_DIR}/nt-x86/bin"
+        # Mercifully msys git has git svn bundled in by default, so the repos toolset is enough.
         ;;
     OSX*)
         ham-brew-install svn "bin/svn"
+        ham-brew-install git-svn "bin/git-svn"
         ;;
     LINUX*)
         if [ -z `which svn` ]; then
             sudo apt install -y subversion
+        fi
+        if [[ ! -e "$(git --exec-path)/git-svn" ]]; then
+            sudo apt install -y git-svn
         fi
         ;;
     *)
@@ -29,7 +35,16 @@ pathenv_add "${HAM_TOOLSET_DIR}"
 
 # version check
 VER="--- svn --------------------------
-`svn --version | grep ", version"`"
+$(svn --version | grep ", version")"
+if [ $? != 0 ]; then
+    echo "E/Can't get SVN version."
+    return 1
+fi
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
+
+VER="--- git-svn ----------------------
+$(git svn --version)"
 if [ $? != 0 ]; then
     echo "E/Can't get SVN version."
     return 1
