eeb09d872ffb7b1620cd58b9984a944a5c251e89
Pierre <pierre@bytecollider.com>
2023-08-15 00:55:52 +0800

    bin: Remove deprecated scripts ;

--- bin/pdflatex.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-REM !! DO NOT add quotes around %~dp0 !!
-set HAM_HOME=%~dp0\..
-set PATH="%~dp0";"%~dp0\nt-x86";%HAM_HOME%\toolsets\repos\nt-x86\git\bin\;%PATH%
-
-base.exe "%~dp0\pdflatex" %*
