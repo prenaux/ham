7cc6b72ec14344e9b7d350422ed16c1c8506d7dd
Pierre <pierre@bytecollider.com>
2023-08-16 23:05:18 +0800

    ham-test-thrift: Add ushScripts rule ;

---
+++ sources/ham/tests/ham-test-thrift/_fix.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+#===== PRELUDE BEGIN ===========
+export SCRIPT_NAME=$(basename "$0")
+export SCRIPT_DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+if [[ -z "$HAM_HOME" ]]; then echo "E/HAM_HOME not set !"; exit 1; fi
+. "$HAM_HOME/bin/ham-bash-setenv.sh"
+. "$HAM_HOME/bin/ham-lint-fix-lib.sh"
+cd "$SCRIPT_DIR"
+#===== PRELUDE END =============
+ham_fix_sh "$@"
