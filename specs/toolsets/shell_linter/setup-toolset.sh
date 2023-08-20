20d21dc602825af8cd4134a47f884d61fa2480a9
Pierre <pierre@bytecollider.com>
2023-08-20 16:20:08 +0800

    toolsets/shell_linter: Add Linux support ;

--- specs/toolsets/shell_linter/setup-toolset.sh
+++ specs/toolsets/shell_linter/setup-toolset.sh
@@ -7,7 +7,7 @@ export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/shell_linter"
 
 # path setup
 case $HAM_OS in
-    OSX*)
+    OSX*|LINUX*)
         toolset_check_and_dl_ver shell_linter ${HAM_BIN_LOA} v1 || return 1
         export HAM_SHELL_LINTER_DIR="${HAM_TOOLSET_DIR}/${HAM_BIN_LOA}"
         pathenv_add "${HAM_SHELL_LINTER_DIR}"
@@ -30,8 +30,8 @@ pathenv_add "${HAM_TOOLSET_DIR}"
 VER="--- shell_linter ------------------------"
 if [ "$HAM_NO_VER_CHECK" != "1" ]; then
     VER="$VER
-$(shellcheck --version | grep "version:")
-$(shfmt --version)"
+Shellcheck: $(shellcheck --version | grep "version:")
+Shfmt: $(shfmt --version)"
     if [ $? != 0 ]; then
       echo "E/Can't get shell_linter version."
       return 1
