5eb3d0a2524bb5ee2bcc9b52c2770f4c756fa8b2
Pierre <pierre@bytecollider.com>
2024-01-07 02:01:50 +0800

    emacs/site/ni-evil.el: Add evil-owl which previews the registers before they are used ;

--- sources/emacs/site/ni-evil.el
+++ sources/emacs/site/ni-evil.el
@@ -5,27 +5,44 @@
 (evil-mode 1)
 
 (setq evil-search-wrap nil)
-(define-key evil-visual-state-map (kbd "y") 'kill-ring-save)
+(setq evil-search-module 'evil-search)
 (evil-select-search-module 'evil-search-module 'evil-search)
 
 (GNUEmacsMin28
   (evil-set-undo-system 'undo-redo))
 
+;;=================================================================
+;; evil-owl (registers preview)
+;;=================================================================
+(require 'evil-owl)
+(setq evil-owl-max-string-length 500)
+(add-to-list 'display-buffer-alist
+  '("*evil-owl*"
+     (display-buffer-in-side-window)
+     (side . bottom)
+     (window-height . 0.3)))
+(evil-owl-mode)
+(setq evil-owl-idle-delay 0.3)
+(diminish 'evil-owl-mode)
+
+;;=================================================================
 ;; Relative line numbers
-(defvar ni-rel-line-current-line-number 0)
+;;=================================================================
+(IsNotTerminal
+  (defvar ni-rel-line-current-line-number 0)
 
-(setq linum-format 'ni-rel-line-numbers)
+  (setq linum-format 'ni-rel-line-numbers)
 
-(defun ni-rel-line-numbers (line-number)
-  (let ((test2 (- line-number ni-rel-line-current-line-number)))
-    (propertize
-     (number-to-string (cond ((<= test2 0) (* -1 test2))
-                             ((> test2 0) test2)))
-     'face 'linum)))
+  (defun ni-rel-line-numbers (line-number)
+    (let ((test2 (- line-number ni-rel-line-current-line-number)))
+      (propertize
+        (number-to-string (cond ((<= test2 0) (* -1 test2))
+                            ((> test2 0) test2)))
+        'face 'linum)))
 
-(defadvice linum-update (around ni-rel-line-update)
-  (let ((ni-rel-line-current-line-number (line-number-at-pos)))
-    ad-do-it))
-(ad-activate 'linum-update)
+  (defadvice linum-update (around ni-rel-line-update)
+    (let ((ni-rel-line-current-line-number (line-number-at-pos)))
+      ad-do-it))
+  (ad-activate 'linum-update)
 
-(global-linum-mode t)
+  (global-linum-mode t))
