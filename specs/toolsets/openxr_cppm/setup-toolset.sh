f0c89f7a1cb7f3d0213ee555ee0cc87de1c5b885
Javier <javierferrergomez@protonmail.com>
2024-03-19 16:22:14 +0800

    toolsets/openxr_cppm: Added openxr toolset

---
+++ specs/toolsets/openxr_cppm/setup-toolset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+export HAM_TOOLSET=openxr_cppm
+export HAM_TOOLSET_NAME=openxr_cppm
+export HAM_TOOLSET_DIR="${HAM_HOME}/specs/toolsets/openxr_cppm"
+
+BUILD=ra ham-cppm-build-check openxr_cppm noheader openxr_cppm openxr_cppm
+BUILD=da ham-cppm-build-check openxr_cppm noheader openxr_cppm openxr_cppm
+
+VER="--- openxr_cppm ---------------------"
+export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
+$VER"
+
