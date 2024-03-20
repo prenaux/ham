971c4de43546a434e07668726395945677985525
Pierre Renaux <pierre@bytecollider.com>
2024-03-20 12:44:23 +0800

    toolsets/openxr_cppm/setup-toolset.sh: Only Windows is tested & supported atm.

--- specs/toolsets/openxr_cppm/setup-toolset.sh
+++ specs/toolsets/openxr_cppm/setup-toolset.sh
@@ -3,10 +3,17 @@ export HAM_TOOLSET=openxr_cppm
 export HAM_TOOLSET_NAME=openxr_cppm
 export HAM_TOOLSET_DIR="${HAM_HOME}/specs/toolsets/openxr_cppm"
 
+case $HAM_OS in
+  NT*) ;;
+  *)
+    echo "E/Toolset: Unsupported host OS"
+    return 1
+    ;;
+esac
+
 BUILD=ra ham-cppm-build-check openxr_cppm noheader openxr_cppm openxr_cppm
 BUILD=da ham-cppm-build-check openxr_cppm noheader openxr_cppm openxr_cppm
 
 VER="--- openxr_cppm ---------------------"
 export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
 $VER"
-
