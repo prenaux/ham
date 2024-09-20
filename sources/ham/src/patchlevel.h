4d07ef961b53cc0b82a36f35827fcdd770dd96c5
pierre <pierre@bytecollider.com>
2024-09-20 20:46:24 +0800

    ham/src: Bump the version number ;

--- sources/ham/src/patchlevel.h
+++ sources/ham/src/patchlevel.h
@@ -1,4 +1,4 @@
 /* Keep JAMVERSYM in sync with VERSION. */
 /* It can be accessed as $(JAMVERSION) in the Jamfile. */
-#define VERSION "21 (" __DATE__ " - " __TIME__ ")"
-#define JAMVERSYM "JAMVERSION=21"
+#define VERSION "22 (" __DATE__ " - " __TIME__ ")"
+#define JAMVERSYM "JAMVERSION=22"
