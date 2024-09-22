77d449409488870e0601af23c89135b0102173be
pierre <pierre@bytecollider.com>
2024-09-22 13:21:09 +0800

    toolsets/linux_x64: Update setup-toolset.sh to provide support for Clang 19 on Linux.

--- specs/toolsets/linux_x64/setup-toolset.sh
+++ specs/toolsets/linux_x64/setup-toolset.sh
@@ -12,24 +12,36 @@ esac
 export OSPLAT=X64
 export BUILD_BIN_LOA=$HAM_BIN_LOA
 
-# Default to Clang 18 because we install it ourselve so its a reliable well
-# defined version.
-export LINUX_CLANG=${LINUX_CLANG:-18}
+if [ -n "$HAM_LINUX_CLANG" ]; then
+  LINUX_CLANG=$HAM_LINUX_CLANG
+else
+  # Default to Clang 19 redist so we have a well know version by default.
+  LINUX_CLANG=clang_19
+fi
 
 # Use GCC
-if [ "${LINUX_CLANG}" == "0" ]; then
+if [ "${LINUX_CLANG}" == "gcc_system" ]; then
+  ### system gcc ###
   toolset_import gcc_470 || return 1
 
   VER="--- linux_x64 -----------------------
-Using GCC System: $(gcc --version | grep gcc)"
+Using system GCC: $(gcc --version | grep gcc)"
 
-elif [ "${LINUX_CLANG}" == "18" ]; then
+elif [ "${LINUX_CLANG}" == "clang_18" ]; then
+  ### clang 18 ###
   toolset_import clang_18 || return 1
 
   VER="--- linux_x64 -----------------------
-Using Clang 18: $(clang -arch x86_64 --version)"
+Using redist Clang 18: $(clang -arch x86_64 --version | grep version)"
 
-else
+elif [ "${LINUX_CLANG}" == "clang_19" ]; then
+  ### clang 19 ###
+  toolset_import clang_19 || return 1
+
+  VER="--- linux_x64 -----------------------
+Using redist Clang 19: $(clang -arch x86_64 --version | grep version)"
+
+elif [ "${LINUX_CLANG}" == "clang_system" ]; then
   # Use the system clang
   export HAM_TOOLSET=CLANG
   export HAM_TOOLSET_NAME=clang_33
@@ -42,10 +54,15 @@ else
   export CMD_JSON_COMPILER_PATH=${dir#*' '}/
 
   if ! VER="--- linux_x64 -----------------------
-Using Clang System: $(clang -arch x86_64 --version)"; then
+Using system Clang: $(clang -arch x86_64 --version)"; then
     echo "E/Can't get version."
     return 1
   fi
+
+else
+  ### Unkown Linux C/C++ compiler ###
+  log_error "Unknown LINUX_CLANG: '$LINUX_CLANG'."
+  return 1
 fi
 
 export HAM_TOOLSET_VERSIONS="$HAM_TOOLSET_VERSIONS
