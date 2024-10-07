fc7ef107feb11852bad28b0e6cb3ff1190fec3c6
Pierre <pierre@bytecollider.com>
2024-10-07 23:34:21 +0800

    toolsets/docker/setup-toolset.sh: Add the docker registry to Apt sources ;

--- specs/toolsets/docker/setup-toolset.sh
+++ specs/toolsets/docker/setup-toolset.sh
@@ -8,7 +8,25 @@ export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/docker"
 case "$HAM_BIN_LOA" in
   lin-x64)
     if [ -z "$(where_inpath docker)" ]; then
-      # TODO: More robust test of whether all of those are installed.
+      log_info "Adding Docker repository to Apt sources..."
+      (
+        set -x
+        sudo apt-get update -y
+        sudo apt-get install -y ca-certificates curl
+        sudo install -m 0755 -d /etc/apt/keyrings
+        sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+        sudo chmod a+r /etc/apt/keyrings/docker.asc
+        sudo apt-get update -y
+      )
+
+      if ! grep -q "^deb .*download.docker.com/linux/ubuntu" /etc/apt/sources.list.d/docker.list 2>/dev/null; then
+        log_info "Adding Docker repo to apt sources.list."
+        echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+          $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+      else
+        log_info "Docker repo already added to apt sources.list."
+      fi
+
       ham-apt-get-install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
     fi
     ;;
