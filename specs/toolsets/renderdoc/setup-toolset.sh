e6cecb85c36a28ff7ea74608e1b6ff3f289665e2
Pierre <pierre@bytecollider.com>
2024-12-20 12:04:01 +0800

    toolsets/renderdoc: Add Windows version of the toolset ; ham-renderdoc, add a gui option ;

--- specs/toolsets/renderdoc/setup-toolset.sh
+++ specs/toolsets/renderdoc/setup-toolset.sh
@@ -6,14 +6,17 @@ export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/renderdoc"
 case $HAM_OS in
   LINUX)
     toolset_check_and_dl_ver renderdoc lin-x64 v1_35 || return 1
+    pathenv_add "${HAM_TOOLSET_DIR}/$HAM_BIN_LOA/bin"
+    ;;
+  NT)
+    toolset_check_and_dl_ver renderdoc nt-x64 v1_35 || return 1
+    pathenv_add "${HAM_TOOLSET_DIR}/nt-x64"
     ;;
   *)
     echo "E/Toolset: Unsupported host OS"
     return 1
     ;;
 esac
-
-pathenv_add "${HAM_TOOLSET_DIR}/$HAM_BIN_LOA/bin"
 
 # version
 VER="--- renderdoc ----------------------------"
