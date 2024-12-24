7be24e379b59992b617a20531e5be48d6fa1b473
pierre <pierre@talansoft.com>
2024-12-24 16:04:37 +0800

    toolsets/alacritty: Fix config not loading correctly ;

--- toolsets/alacritty/alacritty-ham-shell.cmd
+++ toolsets/alacritty/alacritty-ham-shell.cmd
@@ -2,7 +2,7 @@
 setlocal EnableDelayedExpansion
 
 REM !! DO NOT add quotes around %~dp0 !!
-set HAM_HOME=%~dp0\..\..\..
+set HAM_HOME=%~dp0\..\..
 set PATH="%~dp0";"%~dp0\nt-x86";%HAM_HOME%\toolsets\repos\nt-x86\git\bin\;%PATH%
 
 call "%HAM_HOME%\bin\ham-install-for-windows.cmd"
@@ -15,7 +15,7 @@ del "%USERPROFILE%\_alacritty_last_cwd.txt"
 
 set "HAM_HOME_FWD=%HAM_HOME:\=/%"
 
-start "" /min "C:\Utils\Alacritty.exe" --config-file "%HAM_HOME_FWD%/toolsets/alacritty/windows/alacritty-ham-shell.toml" ^
-    -o "shell={program=\"K:/Work/ham/toolsets/repos/nt-x86/git/bin/bash.exe\",args=[\"--rcfile\",\"%HAM_HOME_FWD%/toolsets/alacritty/windows/alacritty-ham-shell-start.sh\",\"-i\"]}"
+start "" /b /min "C:\Utils\Alacritty.exe" --config-file "%HAM_HOME_FWD%/toolsets/alacritty/alacritty-ham-shell.toml" ^
+    -o "shell={program=\"K:/Work/ham/toolsets/repos/nt-x86/git/bin/bash.exe\",args=[\"--rcfile\",\"%HAM_HOME_FWD%/toolsets/alacritty/alacritty-ham-shell-start.sh\",\"-i\"]}"
 
 exit
