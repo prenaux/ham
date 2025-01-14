8b4ed2041aced4859fd7703d93757fcb73dbcc85
Pierre <pierre@talansoft.com>
2025-01-14 10:18:54 +0800

    bin/ham-grep: Improve Windows support ;

--- bin/ham-grep-for-emacs.cmd
+++ bin/ham-grep-for-emacs.cmd
@@ -3,4 +3,4 @@ REM !! DO NOT add quotes around %~dp0 !!
 set HAM_HOME=%~dp0\..
 set PATH="%~dp0";"%~dp0\nt-x86";%HAM_HOME%\toolsets\repos\nt-x86\git\bin\;%PATH%
 
-bash.exe "%~dp0\ham-grep" --no-heading --color never %*
+bash.exe "%~dp0\ham-grep-for-emacs" %*
