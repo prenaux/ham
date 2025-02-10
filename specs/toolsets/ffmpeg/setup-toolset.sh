9b6da24c15f9874b6ab0aad7b661ada41edecf2d
Pierre <pierre@talansoft.com>
2025-02-10 15:25:25 +0800

    toolsets/ffmpeg/setup-toolset.sh: Update Windows FFMpeg to v7_1 ;

--- specs/toolsets/ffmpeg/setup-toolset.sh
+++ specs/toolsets/ffmpeg/setup-toolset.sh
@@ -8,9 +8,9 @@ export HAM_TOOLSET_DIR="${HAM_HOME}/toolsets/ffmpeg"
 # path setup
 case $HAM_OS in
   NT*)
-    toolset_check_and_dl_ver ffmpeg nt-x86 v2 || return 1
-    export FFMPEG_DIR="${HAM_TOOLSET_DIR}/nt-x86/"
-    pathenv_add "${FFMPEG_DIR}"
+    toolset_check_and_dl_ver ffmpeg nt-x64 v7_1 || return 1
+    export FFMPEG_DIR="${HAM_TOOLSET_DIR}/nt-x64/"
+    pathenv_add "${FFMPEG_DIR}/bin"
     ;;
   OSX*)
     toolset_check_and_dl_ver ffmpeg "$HAM_BIN_LOA" v4 || return 1
